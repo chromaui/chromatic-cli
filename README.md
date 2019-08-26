029f9c04b6770ad2e2d602f0d6005e437bb47fdd
Norbert de Langen <ndelangen@me.com>
2019-08-26 17:28:25 +0200

    IMPROVE readme

--- README.md
+++ README.md
@@ -1,3 +1,78 @@
-# Storybook Chromatic
+# Storybook Chromatic & Storybook Chroma
 
-This document is TODO
+A CLI for uploading/connecting your storybook to chromatic/chroma.
+
+## Install
+
+```sh
+yarn add storybook-chromatic
+# or
+yarn add storybook-chroma
+```
+
+## Usage
+
+Add a npm script called `chromatic` to your `package.json`
+
+```
+  "chromatic": "chromatic",
+```
+
+### Main options
+
+```
+--app_code="<your app code>"
+```
+
+You can also use the environment variable: `CHROMATIC_APP_CODE`
+
+### Storybook options
+
+```
+--build-script-name [name], -b  The npm script that builds your Storybook [build-storybook]
+--script-name [name], -s  The npm script that starts your Storybook [storybook]
+--exec <command>, -e  Alternatively, a full command to run to start your storybook
+--do-not-start, -S  Don't attempt to start or build; use if your Storybook is already running
+
+--storybook-port <port>, -p  What port is your Storybook running on (auto detected from -s, if set)
+--storybook-url <url>, -u  Storybook is already running at (external) url (implies -S)'
+--storybook-build-dir, -d     <dirname>  Provide a directory with your built storybook; use if you've already built your storybook
+--storybook-https  Use if Storybook is running on https (auto detected from -s, if set)
+--storybook-cert <path>  Use if Storybook is running on https (auto detected from -s, if set)
+--storybook-key <path>  Use if Storybook is running on https (auto detected from -s, if set)
+--storybook-ca <ca>  Use if Storybook is running on https (auto detected from -s, if set)    
+```
+
+These options are not required, this CLI is 0-config if you have a `build-storybook` script in your `package.json`.
+
+### Chromatic options
+
+```
+--auto-accept-changes [branch]  Accept any (non-error) changes or new stories for this build [only for <branch> if specified]'
+--exit-zero-on-changes [branch]  Use a 0 exit code if changes are detected (i.e. don't stop the build) [only for <branch> if specified]
+--ignore-last-build-on-branch [branch]  Do not use the last build on this branch as a baseline if it is no longer in history (i.e. branch was rebased) [only for <branch> if specified]'
+--preserve-missing  Treat missing stories as unchanged (as opposed to deleted) when comparing to the baseline'
+--no-interactive  Do not prompt for package.json changes
+--only <component:story>  Only run a single story or a glob-style subset of stories (for debugging purposes
+```
+
+### Debug options
+
+```
+--skip  Skip chromatic tests (mark as passing)
+--list  List available stories (for debugging purposes)
+--ci  This build is running on CI, non-interactively (alternatively, pass CI=true)
+--debug  Output more debugging information
+```
+
+## Contributing
+
+Because of the nature of this package: it being a connector between storybook and a commercial service, you may need an app_code to test this locally. Just send us a message at opensource@hichroma.com!
+
+All contributions are welcome!
+
+## Future plans:
+
+- We'd like to unify this so there's just a single package on npm.
+- Migrate to Typescript
+- Deprecate all the storybook options in favour of a sane `--config` flag
--- bin/main.js
+++ bin/main.js
@@ -16,28 +16,13 @@ export async function parseArgv(argv) {
 
     Main options
       --app-code <code>, -a  The code for your app, get from chromatic.com
-      --config <npm script | command | location | url>
-
-    Advanced options
-      --auto-accept-changes [branch]  Accept any (non-error) changes or new stories for this build [only for <branch> if specified]'
-      --exit-zero-on-changes [branch]  Use a 0 exit code if changes are detected (i.e. don't stop the build) [only for <branch> if specified]
-      --ignore-last-build-on-branch [branch]  Do not use the last build on this branch as a baseline if it is no longer in history (i.e. branch was rebased) [only for <branch> if specified]'
-      --preserve-missing  Treat missing stories as unchanged (as opposed to deleted) when comparing to the baseline'
-      --no-interactive  Do not prompt for package.json changes')
-      --only <component:story>  Only run a single story or a glob-style subset of stories (for debugging purposes)'
-
-    Debug options
-      --skip  Skip chromatic tests (mark as passing)')
-      --list  List available stories (for debugging purposes)')
-      --ci  This build is running on CI, non-interactively (alternatively, pass CI=true)')
-      --debug  Output more debugging information')
-
-    Deprecated options
+      
+    Storybook options
       --build-script-name [name], -b  The npm script that builds your Storybook [build-storybook]
       --script-name [name], -s  The npm script that starts your Storybook [storybook]
       --exec <command>, -e  Alternatively, a full command to run to start your storybook
       --do-not-start, -S  Don't attempt to start or build; use if your Storybook is already running
-
+      
       --storybook-port <port>, -p  What port is your Storybook running on (auto detected from -s, if set)?'
       --storybook-url <url>, -u  Storybook is already running at (external) url (implies -S)'
       --storybook-build-dir, -d     <dirname>  Provide a directory with your built storybook; use if you've already built your storybook
@@ -45,15 +30,31 @@ export async function parseArgv(argv) {
       --storybook-cert <path>  Use if Storybook is running on https (auto detected from -s, if set)?'
       --storybook-key <path>  Use if Storybook is running on https (auto detected from -s, if set)?'
       --storybook-ca <ca>  Use if Storybook is running on https (auto detected from -s, if set)?'    
+      
+    Chromatic options
+      --auto-accept-changes [branch]  Accept any (non-error) changes or new stories for this build [only for <branch> if specified]'
+      --exit-zero-on-changes [branch]  Use a 0 exit code if changes are detected (i.e. don't stop the build) [only for <branch> if specified]
+      --ignore-last-build-on-branch [branch]  Do not use the last build on this branch as a baseline if it is no longer in history (i.e. branch was rebased) [only for <branch> if specified]'
+      --preserve-missing  Treat missing stories as unchanged (as opposed to deleted) when comparing to the baseline'
+      --no-interactive  Do not prompt for package.json changes')
+      --only <component:story>  Only run a single story or a glob-style subset of stories (for debugging purposes)'
+      
+    Debug options
+      --skip  Skip chromatic tests (mark as passing)')
+      --list  List available stories (for debugging purposes)')
+      --ci  This build is running on CI, non-interactively (alternatively, pass CI=true)')
+      --debug  Output more debugging information')
     `,
     {
       argv,
       booleanDefault: undefined,
       flags: {
         'app-code': { type: 'string', alias: 'a' },
+
+        // main config option in the future
         config: { type: 'string' },
 
-        // deprecated
+        // to be deprecated in the future
         'build-script-name': { type: 'string', alias: 'b' },
         'script-name': { type: 'string', alias: 's' },
         exec: { type: 'string', alias: 'e' },
@@ -79,6 +80,8 @@ export async function parseArgv(argv) {
         interactive: { type: 'boolean', default: true },
         ci: { type: 'boolean' },
         debug: { type: 'boolean' },
+
+        // feature toggle to use new version (WIP)
         new: { type: 'boolean' },
       },
     }
