fd59cb45c8937cacc32df40d6f35cc4b598066ed
Norbert de Langen <ndelangen@me.com>
2019-09-02 13:34:39 +0200

    FIX serializers
    apply
    https://github.com/chromaui/chromatic/commit/6e434c3250cb9b032dead15236a25ce042ee9f49
    https://github.com/chromaui/chromatic/commit/87f2e80408b14e0554adbd1e940f4f4130843ac0

--- bin/io/serializers.js
+++ bin/io/serializers.js
@@ -6,16 +6,23 @@ export function responseSerializer({ status, statusText, headers, url, _raw }) {
     statusText,
     headers,
     url,
-    _raw: _raw.toString(),
+    ...(_raw && { _raw: _raw.toString() }),
   };
 }
 
+// We *don't* want to log the envPairs key -- this is added by node and includes
+// all of our environment variables! See https://github.com/chromaui/chromatic/issues/1993
+// Note it is added to both err.envPairs *and* err.options.envPairs :facepalm:
+function stripEnvPairs(err) {
+  const { envPairs, options: { envPairs: x, ...options } = {}, ...sanitizedErr } = err;
+  return { sanitizedErr, ...(err.options && { options }) };
+}
+
 export const errSerializer = err => {
-  // We *don't* want to log the envPairs key -- this is added by node and includes
-  // all of our environment variables! See https://github.com/chromaui/chromatic/issues/1993
-  const { envPairs, ...serializedErr } = pino.stdSerializers.err(err);
+  const serializedErr = pino.stdSerializers.err(err);
+
   return {
-    ...serializedErr,
+    ...stripEnvPairs(serializedErr),
     // Serialize the response part of err with the response serializer
     ...(err.response && { response: responseSerializer(err.response) }),
   };
