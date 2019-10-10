3777ba46f9b05264e679353d93e304719fffb513
Norbert de Langen <ndelangen@me.com>
2019-10-10 15:52:42 +0200

    compile to commonjs, because typescript is not playing along

---
+++ typings.d.ts
@@ -0,0 +1,4 @@
+declare module '@actions/core';
+declare module '@actions/github';
+declare module 'storybook-chromatic/bin/tester/index';
+declare module 'storybook-chromatic/bin/lib/verify-option';
