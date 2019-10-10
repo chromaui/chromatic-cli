21f860fdeb3368c5333f2a3065b4f4d8435346d3
Norbert de Langen <ndelangen@me.com>
2019-10-10 14:10:14 +0200

    ADD a deployment, do stuff in chromatic-cli, and complete deployment

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { getInput, info, setFailed, setOutput } from '@actions/core';
-import {GitHub, context} from "@actions/github";
+import { GitHub, context } from "@actions/github";
 
 
 async function run() {
   try {
-    
     const token = getInput('token');
     const github = new GitHub(token);
 
@@ -21,40 +20,30 @@ async function run() {
     const storybookKey = getInput('storybookKey');
     const storybookCa = getInput('storybookCa');
   
-    info((new Date()).toTimeString())
+    const { data: { id }} = await github.repos.createDeployment({
+      ...context.repo,
+      ref: context.sha,
+      environment: 'staging',
+      required_contexts: [],
+    });
 
-    // const { data: { id }} = await github.repos.createDeployment({
-    //   ...context.repo,
-    //   ref: context.sha,
-    //   environment: 'staging',
-    //   required_contexts: [],
-    // });
-
-    // info('id: ' + id);
-
+    info('deployment_id: ' + id);
     
-    // github.repos.createDeploymentStatus({
-    //   ...context.repo,
-    //   deployment_id: id,
-    //   state: "inactive",
-    // });
+    github.repos.createDeploymentStatus({
+      ...context.repo,
+      deployment_id: id,
+      state: "inactive",
+    });
+
+    // let's do some chromatic-cli stuff!
 
     github.repos.createDeploymentStatus({
       ...context.repo,
-      deployment_id: 174461623,
+      deployment_id: id,
       state: "success",
       environment_url: 'https://example.com?real',
       log_url: 'https://example.com?log'
     });
-
-
-    // const newIssue = await github.issues.create({
-    //   ...context.repo,
-    //   title: 'New issue!',
-    //   body: 'Hello Universe!'
-    // });
-
-    // log.info(newIssue)
 
     setOutput('time', JSON.stringify({
       appCode,
