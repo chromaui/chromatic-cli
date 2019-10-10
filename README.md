61af38386ece949cf5e438d555547b4b52dea0a0
Norbert de Langen <ndelangen@me.com>
2019-10-10 18:31:24 +0200

    IMPROVE readme && More error handling

--- README.md
+++ README.md
@@ -5,28 +5,30 @@ We use [GitHub's action toolkit](https://github.com/actions/toolkit/blob/master/
 ## Usage
 
 ```yaml
-uses: chromaui/action@v1
-with:
-  appCode: 'Your chromatic/chroma app_code'
-  buildScriptName: 'The npm script that builds your Storybook [build-storybook]'
-  scriptName: 'The npm script that starts your Storybook [storybook]'
-  exec: 'Alternatively, a full command to run to start your storybook'
-  doNotStart: 'Don't attempt to start or build; use if your Storybook is already running'
-  storybookPort: 'What port is your Storybook running on (auto detected from -s, if set)'
-  storybookUrl: 'Storybook is already running at (external) url (implies -S)'
-  storybookBuildDir: 'Provide a directory with your built storybook; use if you've already built your storybook'
-  storybookHttps: 'Use if Storybook is running on https (auto detected from -s, if set)'
-  storybookCert: 'Use if Storybook is running on https (auto detected from -s, if set)'
-  storybookKey: 'Use if Storybook is running on https (auto detected from -s, if set)'
-  storybookCa: 'Use if Storybook is running on https (auto detected from -s, if set)'
+- uses: chromaui/action@v1
+  with:
+    token: ${{ secrets.GITHUB_TOKEN }}
+    appCode: 'Your chromatic/chroma app_code'
+    buildScriptName: 'The npm script that builds your Storybook [build-storybook]'
+    scriptName: 'The npm script that starts your Storybook [storybook]'
+    exec: 'Alternatively, a full command to run to start your storybook'
+    doNotStart: 'Don't attempt to start or build; use if your Storybook is already running'
+    storybookPort: 'What port is your Storybook running on (auto detected from -s, if set)'
+    storybookUrl: 'Storybook is already running at (external) url (implies -S)'
+    storybookBuildDir: 'Provide a directory with your built storybook; use if you've already built your storybook'
+    storybookHttps: 'Use if Storybook is running on https (auto detected from -s, if set)'
+    storybookCert: 'Use if Storybook is running on https (auto detected from -s, if set)'
+    storybookKey: 'Use if Storybook is running on https (auto detected from -s, if set)'
+    storybookCa: 'Use if Storybook is running on https (auto detected from -s, if set)'
 ```
 
 We suggest you use secret:
 
 ```yaml
-uses: chromaui/action@v1
-with:
-  appCode: ${{ secrets.CHROMATIC_APP_CODE }}
+- uses: chromaui/action@v1
+  with:
+    token: ${{ secrets.GITHUB_TOKEN }}
+    appCode: ${{ secrets.CHROMATIC_APP_CODE }}
 ```
 
 You have to configure secrets in the settings tab (https://github.com/<org>/<repo>/settings/secrets)
@@ -62,9 +64,9 @@ See the [versioning documentation](https://github.com/actions/toolkit/blob/maste
 You can now validate the action by referencing the releases/v1 branch
 
 ```yaml
-uses: chromaui/action@releases/v1
-with:
-  appCode: ${{ secrets.CHROMATIC_APP_CODE }}
+- uses: chromaui/action@releases/v1
+  with:
+    appCode: ${{ secrets.CHROMATIC_APP_CODE }}
 ```
 
 See the [actions tab](https://github.com/chromaui/action/actions) for runs of this action! :rocket:
@@ -74,7 +76,7 @@ See the [actions tab](https://github.com/chromaui/action/actions) for runs of th
 After testing you can [create a v1 tag](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md) to reference the stable and tested action
 
 ```yaml
-uses: chromaui/action@v1
-with:
-  appCode: ${{ secrets.CHROMATIC_APP_CODE }}
+- uses: chromaui/action@v1
+  with:
+    appCode: ${{ secrets.CHROMATIC_APP_CODE }}
 ```
--- src/main.ts
+++ src/main.ts
@@ -4,102 +4,105 @@ import { runTest } from 'storybook-chromatic/bin/tester/index';
 import { verifyOptions } from 'storybook-chromatic/bin/lib/verify-option';
 
 async function run() {
-  const token = getInput('token');
-  const github = new GitHub(token);
-
-  const appCode = getInput('appCode');
-  const buildScriptName = getInput('buildScriptName');
-  const scriptName = getInput('scriptName');
-  const exec = getInput('exec');
-  const doNotStart = getInput('doNotStart');
-  const storybookPort = getInput('storybookPort');
-  const storybookUrl = getInput('storybookUrl');
-  const storybookBuildDir = getInput('storybookBuildDir');
-  const storybookHttps = getInput('storybookHttps');
-  const storybookCert = getInput('storybookCert');
-  const storybookKey = getInput('storybookKey');
-  const storybookCa = getInput('storybookCa');
-
-  const { data: { id }} = await github.repos.createDeployment({
-    ...context.repo,
-    ref: context.sha,
-    environment: 'staging',
-    required_contexts: [],
-  });
-  
-  github.repos.createDeploymentStatus({
-    ...context.repo,
-    deployment_id: id,
-    state: "in_progress",
-  });
-
   try {
-    info('options: ' + JSON.stringify({
-        buildScriptName,
-        scriptName,
-        exec,
-        doNotStart,
-        storybookPort,
-        storybookUrl,
-        storybookBuildDir,
-        storybookHttps,
-        storybookCert,
-        storybookKey,
-        storybookCa,
-      }, null, 2));
+    const token = getInput('token');
+    const github = new GitHub(token);
+
+    const appCode = getInput('appCode');
+    const buildScriptName = getInput('buildScriptName');
+    const scriptName = getInput('scriptName');
+    const exec = getInput('exec');
+    const doNotStart = getInput('doNotStart');
+    const storybookPort = getInput('storybookPort');
+    const storybookUrl = getInput('storybookUrl');
+    const storybookBuildDir = getInput('storybookBuildDir');
+    const storybookHttps = getInput('storybookHttps');
+    const storybookCert = getInput('storybookCert');
+    const storybookKey = getInput('storybookKey');
+    const storybookCa = getInput('storybookCa');
+
+    const { data: { id }} = await github.repos.createDeployment({
+      ...context.repo,
+      ref: context.sha,
+      environment: 'staging',
+      required_contexts: [],
+    });
     
+    github.repos.createDeploymentStatus({
+      ...context.repo,
+      deployment_id: id,
+      state: "in_progress",
+    });
 
-    const maybe = (a) => {
-      if(!a) {
-        return undefined;
+    try {
+      info('options: ' + JSON.stringify({
+          buildScriptName,
+          scriptName,
+          exec,
+          doNotStart,
+          storybookPort,
+          storybookUrl,
+          storybookBuildDir,
+          storybookHttps,
+          storybookCert,
+          storybookKey,
+          storybookCa,
+        }, null, 2));
+      
+
+      const maybe = (a) => {
+        if(!a) {
+          return undefined;
+        }
+
+        try {
+          return JSON.parse(a);
+        } catch(e){
+          return a;
+        }
       }
 
-      try {
-        return JSON.parse(a);
-      } catch(e){
-        return a;
-      }
+      const exitCode = await runTest(verifyOptions({
+        appCode,
+        buildScriptName: maybe(buildScriptName),
+        scriptName: maybe(scriptName),
+        exec: maybe(exec),
+        doNotStart: maybe(doNotStart),
+        storybookPort: maybe(storybookPort),
+        storybookUrl: maybe(storybookUrl),
+        storybookBuildDir: maybe(storybookBuildDir),
+        storybookHttps: maybe(storybookHttps),
+        storybookCert: maybe(storybookCert),
+        storybookKey: maybe(storybookKey),
+        storybookCa: maybe(storybookCa),
+      }));
+      const url = 'https://example.com?real'
+      const status = exitCode === 0 ? 'success' : 'failure';
+
+      github.repos.createDeploymentStatus({
+        ...context.repo,
+        deployment_id: id,
+        state: status,
+        environment_url: url,
+        log_url: 'https://example.com?log'
+      });
+
+      setOutput('url', url);
+    } catch (e) {
+      e.message && error(e.message);
+      e.stack && error(e.stack);
+      e.description && error(e.description);
+
+      github.repos.createDeploymentStatus({
+        ...context.repo,
+        deployment_id: id,
+        state: 'failure',
+      });
+
+      setFailed(e.message);
     }
-
-    const exitCode = await runTest(verifyOptions({
-      appCode,
-      buildScriptName: maybe(buildScriptName),
-      scriptName: maybe(scriptName),
-      exec: maybe(exec),
-      doNotStart: maybe(doNotStart),
-      storybookPort: maybe(storybookPort),
-      storybookUrl: maybe(storybookUrl),
-      storybookBuildDir: maybe(storybookBuildDir),
-      storybookHttps: maybe(storybookHttps),
-      storybookCert: maybe(storybookCert),
-      storybookKey: maybe(storybookKey),
-      storybookCa: maybe(storybookCa),
-    }));
-    const url = 'https://example.com?real'
-    const status = exitCode === 0 ? 'success' : 'failure';
-
-    github.repos.createDeploymentStatus({
-      ...context.repo,
-      deployment_id: id,
-      state: status,
-      environment_url: url,
-      log_url: 'https://example.com?log'
-    });
-
-    setOutput('url', url);
   } catch (e) {
-    e.message && error(e.message);
-    e.stack && error(e.stack);
-    e.description && error(e.description);
-
-    github.repos.createDeploymentStatus({
-      ...context.repo,
-      deployment_id: id,
-      state: 'failure',
-    });
-
     setFailed(e.message);
   }
 }
-
 run();
