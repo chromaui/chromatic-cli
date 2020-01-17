0cf6cc3ef1ec0cfd46770ac99889a66a2d14c988
Norbert de Langen <ndelangen@me.com>
2020-01-17 22:23:34 +0100

    ADD a test if the buildDir exists (and has an iframe.html) and throw if missing

--- bin/tester/index.js
+++ bin/tester/index.js
@@ -1,4 +1,6 @@
 import fetch from 'node-fetch';
+import fs from 'fs-extra';
+import path from 'path';
 import denodeify from 'denodeify';
 import { confirm } from 'node-ask';
 import setupDebug from 'debug';
@@ -103,12 +105,22 @@ async function prepareAppOrBuild({
       await new Promise((res, rej) => {
         child.on('error', rej);
         child.on('close', code => {
-          if (code > 0) {
+          if (code !== 0) {
             rej(new Error(`${buildScriptName} script exited with code ${code}`));
           }
           res();
         });
       });
+    }
+
+    const exists = await fs.pathExists(path.join(buildDirName, 'iframe.html'));
+
+    if (!exists) {
+      if (buildScriptName) {
+        throw new Error(`Storybook was not build succesfully, there are likely errors above`);
+      } else {
+        throw new Error(`The folder you specified (${buildDirName}) doesn't exist`);
+      }
     }
 
     log.info(dedent`Uploading your built Storybook...`);
