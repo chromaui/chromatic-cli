dbec364eb6deb3729152024b89d3d48f5b5ad4b1
Gert Hengeveld <info@ghengeveld.nl>
2020-05-25 13:14:11 +0200

    More consistent whitespace.

--- bin/ui/messages/errors/buildHasChanges.js
+++ bin/ui/messages/errors/buildHasChanges.js
@@ -9,10 +9,8 @@ export default ({ build, exitCode }) => {
   const changes = pluralize('visual changes', build.changeCount, true);
   return dedent(chalk`
     ${error} {bold Found ${changes}}: failing with exit code ${exitCode}
-
     Pass {bold --exit-zero-on-changes} to succeed this command regardless of changes.
     Pass {bold --auto-accept-changes} to succeed and automatically accept any changes.
-
     ${info} Read more at ${link('https://www.chromatic.com/docs/test')}
   `);
 };
