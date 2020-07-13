2c956bf1df38fd3ba7a39c76d1fe8af45e963334
Norbert de Langen <ndelangen@me.com>
2020-07-13 16:22:51 +0200

    FIX after upgrading to chromatic v5 package

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { getInput, error, setFailed, setOutput } from '@actions/core';
 import { context } from "@actions/github";
-import { runTest } from 'chromatic/bin/tester/index';
-import { verifyOptions } from 'chromatic/bin/lib/verify-option';
+import { v4 as uuid } from 'uuid';
+
+import { runAll } from 'chromatic/bin/main';
+import parseArgs from 'chromatic/bin/lib/parseArgs';
+import {createLogger} from 'chromatic/bin/lib/log';
+import getEnv from 'chromatic/bin/lib/getEnv';
 
 const maybe = (a: string, b: any = undefined) => {
   if(!a) {
@@ -56,7 +60,15 @@ interface Output {
 }
 
 async function runChromatic(options): Promise<Output> {
-  const { exitCode, exitUrl } = await runTest(await verifyOptions(options));  
+  const sessionId = uuid();
+  const env = getEnv();
+  const log = createLogger(sessionId, env);
+
+  const context = {...parseArgs([]), env, log, sessionId, flags: options} as any
+  
+  await runAll(context);  
+
+  const { build: { webUrl: exitUrl }, exitCode } = context;
 
   return {
     url: exitUrl,
