58745e8e5f02d6c26d7f112b2980ca1f642274a8
Gert Hengeveld <info@ghengeveld.nl>
2020-07-22 11:54:37 +0200

    Add warning about chaining commands.

--- bin/ui/messages/warnings/deviatingOutputDir.stories.js
+++ bin/ui/messages/warnings/deviatingOutputDir.stories.js
@@ -4,11 +4,22 @@ export default {
   title: 'CLI/Messages/Warnings',
 };
 
+const withCustomScript = { scripts: { 'build:storybook': './run-storybook-build' } };
+const withChainedScript = { scripts: { 'build:storybook': 'build-storybook && true' } };
+const withNpmRunScript = { scripts: { 'build:storybook': 'npm run build-storybook' } };
+
 const ctx = {
   sourceDir: '/var/folders/h3/ff9kk23958l99z2qbzfjdlxc0000gn/T/chromatic-20036LMP9FAlLEjpu',
   options: { buildScriptName: 'build:storybook' },
-  packageJson: { scripts: { 'build:storybook': 'npm run build-storybook' } },
 };
 
+const outputDir = '/users/me/project/storybook-static';
+
 export const DeviatingOutputDir = () =>
-  deviatingOutputDir(ctx, '/users/me/project/storybook-static');
+  deviatingOutputDir({ ...ctx, packageJson: withCustomScript }, outputDir);
+
+export const DeviatingOutputDirChained = () =>
+  deviatingOutputDir({ ...ctx, packageJson: withChainedScript }, outputDir);
+
+export const DeviatingOutputDirNpmRun = () =>
+  deviatingOutputDir({ ...ctx, packageJson: withNpmRunScript }, outputDir);
