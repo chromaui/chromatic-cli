00bac48473ed426fdb10a182710effc40c325d99
Norbert de Langen <ndelangen@me.com>
2020-08-26 15:27:01 +0200

    ADD skip input option

--- src/main.ts
+++ src/main.ts
@@ -91,6 +91,7 @@ async function run() {
     const buildScriptName = getInput('buildScriptName');
     const scriptName = getInput('scriptName');
     const exec = getInput('exec');
+    const skip = getInput('skip');
     const doNotStart = getInput('doNotStart');
     const storybookPort = getInput('storybookPort');
     const storybookUrl = getInput('storybookUrl');
@@ -114,6 +115,7 @@ async function run() {
       buildScriptName: maybe(buildScriptName),
       scriptName: maybe(scriptName),
       exec: maybe(exec),
+      skip: maybe(skip),
       doNotStart: maybe(doNotStart),
       storybookPort: maybe(storybookPort),
       storybookUrl: maybe(storybookUrl),
