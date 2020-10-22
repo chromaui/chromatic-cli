1c730bb3f717aa2ed93c71b150df9312d384436b
Norbert de Langen <ndelangen@me.com>
2020-10-22 15:01:30 +0200

    test

--- bin/git/git.js
+++ bin/git/git.js
@@ -88,7 +88,7 @@ export async function getCommit() {
     await execGitCommand(`git log -n 1 --format="%H,%ct,%ce,%cn"`)
   ).split(',');
 
-  console.log({ commit, committedAt: committedAtSeconds * 1000, committerEmail, committerName });
+  console.log(await execGitCommand(`git log -n 1 --format="%H,%ct,%ce,%cn" --skip=1`));
 
   return { commit, committedAt: committedAtSeconds * 1000, committerEmail, committerName };
 }
