53705007e224497ea0c2eda04abe3bf819338971
Adrian Macneil <adrian@foxglove.dev>
2021-03-03 17:24:54 -0800

    Support yarn execpath

--- bin/tasks/build.test.js
+++ bin/tasks/build.test.js
@@ -47,6 +47,17 @@ describe('setSpawnParams', () => {
       },
     });
   });
+
+  it('supports yarn', async () => {
+    process.env.npm_execpath = '/path/to/yarn';
+    const ctx = { sourceDir: './source-dir/', options: { buildScriptName: 'build:storybook' } };
+    await setSpawnParams(ctx);
+    expect(ctx.spawnParams).toEqual({
+      command: '/path/to/yarn',
+      clientArgs: ['run', '--silent'],
+      scriptArgs: ['build:storybook', '--output-dir', './source-dir/'],
+    });
+  });
 });
 
 describe('buildStorybook', () => {
