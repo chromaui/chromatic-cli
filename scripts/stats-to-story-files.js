ebfd5b37e352ccdb1cc6b8b38b9260639a73df06
Gert Hengeveld <info@ghengeveld.nl>
2021-04-08 12:27:07 +0200

    Rename file and add script

--- scripts/stats-to-story-files.js
+++ scripts/stats-to-story-files.js
@@ -1,12 +1,11 @@
 import fs from 'fs-extra';
-import path from 'path';
 import { getDependentStoryFiles } from '../bin/lib/getDependentStoryFiles';
 
-const statsPath = path.join(__dirname, '../storybook-static/preview-stats.json');
+const statsFile = process.argv[2] || './storybook-static/preview-stats.json';
 const changedFiles = [];
 
 const main = async () => {
-  const stats = await fs.readJson(statsPath);
+  const stats = await fs.readJson(statsFile);
   const onlyStoryFiles = getDependentStoryFiles(changedFiles, stats);
   console.log(onlyStoryFiles);
 };
