289b9b5b0811f1107a16d2f06da218fa4e9de6b9
Gert Hengeveld <info@ghengeveld.nl>
2021-04-23 15:20:27 +0200

    Update bin/ui/messages/errors/noViewLayerPackage.js
    Co-authored-by: Dominic Nguyen <dom@hichroma.com>

--- bin/ui/messages/errors/noViewLayerPackage.js
+++ bin/ui/messages/errors/noViewLayerPackage.js
@@ -7,5 +7,5 @@ export default (pkg) =>
   dedent(chalk`
     ${error} {bold Storybook package not installed}
     Could not find {bold ${pkg}} in {bold node_modules}.
-    Most likely you forgot to run {bold npm install} or {bold yarn} before running Chromatic.
+    Most likely, you forgot to run {bold npm install} or {bold yarn} before running Chromatic.
   `);
