2953cb2dd4e63c85a76c4d3a9a944b9f9496804c
Gert Hengeveld <info@ghengeveld.nl>
2021-05-31 11:47:12 +0200

    Bail onlyChanged on changes in externals

---
+++ bin/ui/messages/warnings/externalsChanged.stories.js
@@ -0,0 +1,7 @@
+import externalsChanged from './externalsChanged';
+
+export default {
+  title: 'CLI/Messages/Warnings',
+};
+
+export const ExternalsChanged = () => externalsChanged('./styles/main.scss');
