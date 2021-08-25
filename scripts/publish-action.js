a53b818022a75846d2ff2ef64a17f076d51d9b71
Norbert de Langen <ndelangen@me.com>
2021-08-25 21:43:55 +0200

    update publish script to not install anything

--- scripts/publish-action.js
+++ scripts/publish-action.js
@@ -21,23 +21,9 @@ const tmp = require('tmp-promise');
   const { path, cleanup } = await tmp.dir({ unsafeCleanup: true, prefix: `chromatic-action-` });
   const run = (cmd, opts) => execa.command(cmd, { cwd: path, stdio: 'inherit', ...opts });
 
-  await cpy(
-    [
-      'bin/**',
-      '!bin/**/mocks',
-      '!bin/**/*.test.js',
-      '!bin/register.js',
-      'action/main.js',
-      'action/register.js',
-      'action.yml',
-      'package.json',
-    ],
-    path,
-    { parents: true }
-  );
+  await cpy(['action/main.js', 'action.yml', 'package.json'], path, { parents: true });
   await cpy(['action/CHANGELOG.md', 'action/LICENSE', 'action/README.md'], path);
 
-  await run('yarn install --production --no-lockfile');
   await run('git init');
   await run(`git remote add origin git@github.com:${repo}.git`);
   await run('git add .');
