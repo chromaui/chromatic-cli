4d0415391d30ac2fb4424ec362aea844487429ee
Norbert de Langen <ndelangen@me.com>
2021-08-25 13:00:55 +0200

    add bundle script

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 module.exports = {
   entry: './bin-src/main.js',
+  externalsPresets: {node:true},
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'bin'),
   },
 };
