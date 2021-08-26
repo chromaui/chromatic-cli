0cbc69104a5633069996f2dfafd0f29c6b0c672c
Norbert de Langen <ndelangen@me.com>
2021-08-26 10:53:08 +0200

    add the util features back

--- bin-src/register.js
+++ bin-src/register.js
@@ -1,8 +1,25 @@
 #!/usr/bin/env node
+/* eslint-disable global-require */
 import Observable from 'zen-observable';
 
 global.Observable = Observable;
 require('any-observable/register')('global.Observable');
 
 require('dotenv').config();
-require('./main').main(process.argv.slice(2));
+
+const util = ['trim-stats-file', 'stats-to-story-files'].find((u) => u === process.argv[2]);
+
+switch (true) {
+  case util === 'trim-stats-file': {
+    require('./trim-stats-file').main(process.argv.slice(3));
+    break;
+  }
+  case util === 'stats-to-story-files': {
+    require('./stats-to-story-files').main(process.argv.slice(3));
+    break;
+  }
+  default: {
+    require('./main').main(process.argv.slice(2));
+    break;
+  }
+}
