7ed33a67d390530212d1e614a63df506367fc2c0
Norbert de Langen <ndelangen@me.com>
2021-09-22 11:19:19 +0200

    make isChromatic esm, remove the dist

--- src/isChromatic.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env browser */
-
-export default function isChromatic() {
-  return !!(
-    typeof window !== 'undefined' &&
-    (window.navigator.userAgent.match(/Chromatic/) || window.location.href.match(/chromatic=true/))
-  );
-}
