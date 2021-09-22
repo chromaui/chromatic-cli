e645481c274dd78c34751dfd5e00b93c419b79bd
Norbert de Langen <ndelangen@me.com>
2021-09-22 10:38:16 +0200

    fix linting

--- scripts/publish-action.cjs
+++ scripts/publish-action.cjs
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+// eslint-disable-next-line import/no-extraneous-dependencies
 const cpy = require('cpy');
 const execa = require('execa');
 const { readJSON } = require('fs-extra');
