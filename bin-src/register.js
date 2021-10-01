733a7364373d008f9dcf6b270470a1e79e771c48
Norbert de Langen <ndelangen@me.com>
2021-10-01 14:37:03 +0200

    fix the issue where require didn't work anymore

--- bin-src/register.js
+++ bin-src/register.js
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
-import Observable from 'zen-observable';
+/* eslint-disable global-require */
+const Observable = require('zen-observable');
 
 global.Observable = Observable;
-(await import('any-observable/register')).default('global.Observable');
+require('any-observable/register')('global.Observable');
 
-(await import('dotenv')).default.config();
+require('dotenv').config();
 
 const util = ['trim-stats-file', 'stats-to-story-files'].find((u) => u === process.argv[2]);
 
 switch (true) {
   case util === 'trim-stats-file': {
-    (await import('./trim-stats-file')).main(process.argv.slice(3));
+    require('./trim-stats-file').main(process.argv.slice(3));
     break;
   }
   case util === 'stats-to-story-files': {
-    (await import('./stats-to-story-files')).main(process.argv.slice(3));
+    require('./stats-to-story-files').main(process.argv.slice(3));
     break;
   }
   default: {
-    (await import('./main')).main(process.argv.slice(2));
+    require('./main').main(process.argv.slice(2));
     break;
   }
 }
