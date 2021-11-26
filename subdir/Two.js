9d390ff6a99833a20200d34761ad5a7598f03b16
Gert Hengeveld <info@ghengeveld.nl>
2021-11-26 11:33:44 +0100

    Add demo Storybook in a subdirectory

---
+++ subdir/Two.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { One } from './One';
+
+export const Two = ({ children }) => <One>Two {children}</One>;
---
+++ subdir/Two.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Two } from './Two';
+
+export default { title: 'Two' };
+
+export const Default = () => <Two />;
