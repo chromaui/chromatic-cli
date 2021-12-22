49ef6efc94c9b6afd94c15dbcc4d93f00dbb1201
Gert Hengeveld <info@ghengeveld.nl>
2021-12-22 11:20:31 +0100

    Use flags.diagnostics because parsing the options can fail and we still want to write diagnostics if that happens

--- bin-src/lib/getOptions.js
+++ bin-src/lib/getOptions.js
@@ -42,7 +42,6 @@ export default async function getOptions({ argv, env, flags, log, packageJson })
     list: flags.list,
     fromCI,
     skip: trueIfSet(flags.skip),
-    diagnostics: !!flags.diagnostics,
     dryRun: !!flags.dryRun,
     verbose: !!flags.debug,
     interactive: !flags.debug && !fromCI && !!flags.interactive && !!process.stdout.isTTY,
