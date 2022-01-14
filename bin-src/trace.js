75ceaf4fbde9b3510601c28b44671b988fba55c1
Gert Hengeveld <info@ghengeveld.nl>
2022-01-14 10:55:26 +0100

    Update help

--- bin-src/trace.js
+++ bin-src/trace.js
@@ -4,14 +4,27 @@ import { getDependentStoryFiles } from './lib/getDependentStoryFiles';
 
 /**
  * Utility to trace a set of changed file paths to dependent story files using a Webpack stats file.
- * Given a path to `preview-stats.json` and a list of "changed" file paths, it returns a set of
- * story files that depend on any of the changed files.
+ * Given a list of "changed" file paths, it returns a set of story files that depend on any of the
+ * changed files. This report is also available when running a build by passing `--trace-changed`.
  *
  * Command:
- *   chromatic trace [...changed file paths]
+ *   chromatic trace [-b|--base-dir] [-c|--config-dir] [-s|--stats-file] [-u|--untraced] [-m|--mode] [<changed files>...]
  *
- * Usage:
- *   yarn chromatic trace -s ./path/to/preview-stats.json ./src/button.js ./src/header.js
+ * Usage example:
+ *   npx chromatic trace -s ./path/to/preview-stats.json ./src/button.js ./src/header.js
+ *
+ * Example output:
+ *   ℹ Traced 2 changed files to 1 affected story file:
+ *   — src/button.js [changed]
+ *     ∟ src/button.stories.tsx
+ *       ∟ [story index]
+ *
+ * You can set the --mode (-m) flag to change the verbosity:
+ *   - compact: prints only the list of dependent story files, not how they're connected
+ *   - expanded: prints the underlying files for each bundle
+ *
+ * You can generate a preview-stats.json like so (requires Storybook >=6.3):
+ *   npx build-storybook --webpack-stats-json
  */
 
 const { STORYBOOK_BASE_DIR, STORYBOOK_CONFIG_DIR, WEBPACK_STATS_FILE } = process.env;
@@ -20,10 +33,10 @@ export async function main(argv) {
   const { flags, input } = meow(
     `
     Usage
-      $ chromatic trace [-b|--base-dir] [-c|--config-dir] [-s|--stats-file] [-u|--untraced] [-m|--mode] [<files>...]
+      $ chromatic trace [-b|--base-dir] [-c|--config-dir] [-s|--stats-file] [-u|--untraced] [-m|--mode] [<changed files>...]
 
     Options
-      <files>...  List of changed files relative to repository root.
+      <changed files>...  List of changed files relative to repository root.
       --base-dir <dirname>, -b  Relative path from repository root to Storybook project root. Alternatively, set STORYBOOK_BASE_DIR. Use when your Storybook is located in a subdirectory of your repository.
       --config-dir <dirname>, -c  Directory where to load Storybook configurations from. Alternatively, set STORYBOOK_CONFIG_DIR. (default: '.storybook')
       --stats-file <filepath>, -s  Path to preview-stats.json. Alternatively, set WEBPACK_STATS_FILE. (default: 'storybook-static/preview-stats.json')
