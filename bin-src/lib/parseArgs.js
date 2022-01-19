bdab6edfb10c84409b4627bb4f7f109a500f167e
Gert Hengeveld <info@ghengeveld.nl>
2022-01-19 12:04:52 +0100

    Sort alphabetically and move --ci up to Chromatic options

--- bin-src/lib/parseArgs.js
+++ bin-src/lib/parseArgs.js
@@ -22,10 +22,10 @@ export default function parseArgs(argv) {
     Chromatic options
       --auto-accept-changes [branch]            If there are any changes to the build, automatically accept them. Only for [branch], if specified. Globs are supported via picomatch.
       --branch-name <branch>                    Override the branch name. Only meant to be used for unsupported CI integrations and fixing cross-fork PR comparisons. Also accepts <owner>:<branch> format.
+      --ci                                      Mark this build as a CI build. Alternatively, set the 'CI' environment variable (present in most CI systems). This option implies --no-interactive.
       --exit-once-uploaded [branch]             Exit with 0 once the built version has been published to Chromatic. Only for [branch], if specified. Globs are supported via picomatch.
       --exit-zero-on-changes [branch]           If all snapshots render but there are visual changes, exit with code 0 rather than the usual exit code 1. Only for [branch], if specified. Globs are supported via picomatch.
       --externals <filepath>                    Disable TurboSnap when any of these files have changed since the baseline build. Globs are supported via picomatch. This flag can be specified multiple times. Requires --only-changed.
-      --untraced <filepath>                     Disregard these files and their dependencies when tracing dependent stories for TurboSnap. Globs are supported via picomatch. This flag can be specified multiple times. Requires --only-changed.
       --ignore-last-build-on-branch <branch>    Do not use the last build on this branch as a baseline if it is no longer in history (i.e. branch was rebased). Globs are supported via picomatch.
       --only-changed [branch]                   Enables TurboSnap: Only run stories affected by files changed since the baseline build. Only for [branch], if specified. Globs are supported via picomatch. All other snapshots will be inherited from the prior commit.
       --patch-build <headbranch...basebranch>   Create a patch build to fix a missing PR comparison.
@@ -33,10 +33,10 @@ export default function parseArgs(argv) {
       --skip [branch]                           Skip Chromatic tests, but mark the commit as passing. Avoids blocking PRs due to required merge checks. Only for [branch], if specified. Globs are supported via picomatch.
       --storybook-base-dir <dirname>            Relative path from repository root to Storybook project root. Use with --only-changed and --storybook-build-dir when your Storybook is located in a subdirectory of your repository.
       --storybook-config-dir <dirname>          Relative path from where you run Chromatic to your Storybook config directory ('.storybook'). Use with --only-changed and --storybook-build-dir when using a custom --config-dir (-c) flag for Storybook. [.storybook]
+      --untraced <filepath>                     Disregard these files and their dependencies when tracing dependent stories for TurboSnap. Globs are supported via picomatch. This flag can be specified multiple times. Requires --only-changed.
       --zip                                     Publish your Storybook to Chromatic as a single zip file instead of individual content files.
 
     Debug options
-      --ci                          Mark this build as a CI build. Alternatively, set the CI environment variable (present in most CI systems). This option implies --no-interactive.
       --debug                       Output verbose debugging information. This option implies --no-interactive.
       --diagnostics                 Write process context information to chromatic-diagnostics.json.
       --dry-run                     Run without actually publishing to Chromatic.
@@ -65,8 +65,8 @@ export default function parseArgs(argv) {
       description: false,
       flags: {
         // Required options
-        projectToken: { type: 'string', alias: 't', isMultiple: true },
         appCode: { type: 'string', alias: 'a', isMultiple: true }, // for backwards compatibility
+        projectToken: { type: 'string', alias: 't', isMultiple: true },
 
         // Storybook options
         buildScriptName: { type: 'string', alias: 'b' },
@@ -75,23 +75,23 @@ export default function parseArgs(argv) {
 
         // Chromatic options
         autoAcceptChanges: { type: 'string' },
+        branchName: { type: 'string' },
+        ci: { type: 'boolean' },
         exitOnceUploaded: { type: 'string' },
         exitZeroOnChanges: { type: 'string' },
+        externals: { type: 'string', isMultiple: true },
         ignoreLastBuildOnBranch: { type: 'string' },
         only: { type: 'string' },
         onlyChanged: { type: 'string' },
-        untraced: { type: 'string', isMultiple: true },
-        externals: { type: 'string', isMultiple: true },
-        branchName: { type: 'string' },
         patchBuild: { type: 'string' },
         preserveMissing: { type: 'boolean' },
         skip: { type: 'string' },
         storybookBaseDir: { type: 'string' },
         storybookConfigDir: { type: 'string' },
+        untraced: { type: 'string', isMultiple: true },
         zip: { type: 'boolean' },
 
         // Debug options
-        ci: { type: 'boolean' },
         debug: { type: 'boolean' },
         diagnostics: { type: 'boolean' },
         dryRun: { type: 'boolean' },
