3863ea1f9d374e66547a5d68abf9f976171bfbfb
Gert Hengeveld <info@ghengeveld.nl>
2022-01-24 16:44:32 +0100

    Fix story

--- bin-src/ui/messages/errors/gitOneCommit.ts
+++ bin-src/ui/messages/errors/gitOneCommit.ts
@@ -4,18 +4,18 @@ import { dedent } from 'ts-dedent';
 import { error, info } from '../../components/icons';
 import link from '../../components/link';
 
-const githubActionNote = dedent(chalk`
-  In {bold GitHub Actions}, you can enable this by setting \`fetch-depth: 0\`.
-  ${info} Read more at ${link('https://www.chromatic.com/docs/github-actions')}
-`);
-const genericNote = dedent`
-  Refer to your CI provider's documentation for details.
-`;
-
 export default (isGithubAction = false) =>
-  dedent(chalk`
-    ${error} {bold Found only one commit}
-    This typically means you've checked out a shallow copy of the Git repository, which some CI systems do by default.
-    In order for Chromatic to correctly determine baseline commits, we need access to the full Git history graph.
-    ${isGithubAction ? githubActionNote : genericNote}
-  `);
+  isGithubAction
+    ? dedent(chalk`
+      ${error} {bold Found only one commit}
+      This typically means you've checked out a shallow copy of the Git repository, which some CI systems do by default.
+      In order for Chromatic to correctly determine baseline commits, we need access to the full Git history graph.
+      In {bold GitHub Actions}, you can enable this by setting \`fetch-depth: 0\`.
+      ${info} Read more at ${link('https://www.chromatic.com/docs/github-actions')}
+    `)
+    : dedent(chalk`
+      ${error} {bold Found only one commit}
+      This typically means you've checked out a shallow copy of the Git repository, which some CI systems do by default.
+      In order for Chromatic to correctly determine baseline commits, we need access to the full Git history graph.
+      Refer to your CI provider's documentation for details.
+    `);
