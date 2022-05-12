d55c3684856531ce177f0a5a064b00cc385b0b9e
Tom Coleman <tom@chromatic.com>
2022-05-12 13:02:12 +1000

    Added `findAncestorBuildWithCommit` to search the index for a build with a commit

--- bin-src/git/git.test.ts
+++ bin-src/git/git.test.ts
@@ -6,7 +6,13 @@ import tmp from 'tmp-promise';
 import { promisify } from 'util';
 
 import generateGitRepository from './generateGitRepository';
-import { getParentCommits, getCommit, getSlug } from './git';
+import {
+  getParentCommits,
+  getCommit,
+  getSlug,
+  AncestorBuildsQueryResult,
+  findAncestorBuildWithCommit,
+} from './git';
 import longLineDescription from './mocks/long-line';
 import longLoopDescription from './mocks/long-loop';
 import createMockIndex from './mocks/mock-index';
@@ -474,5 +480,65 @@ describe('getSlug', () => {
       () => Promise.resolve({ all: 'https://gitlab.com/foo/bar.baz.git' }) as any
     );
     expect(await getSlug()).toBe('foo/bar.baz');
+  });
+});
+
+describe('findAncestorBuildWithCommit', () => {
+  const client = { runQuery: jest.fn() } as any;
+  beforeEach(() => {
+    execaCommand.mockReset().mockImplementation((command) => {
+      if (command.match(/exists/)) {
+        return Promise.resolve('worked!') as any; // it doesn't matter what it returns
+      }
+      return Promise.reject(new Error('error')) as any;
+    });
+    client.runQuery.mockReset();
+  });
+
+  type Build = AncestorBuildsQueryResult['app']['build']['ancestorBuilds'][0];
+  const makeBuild = (build: Partial<Build> = {}): Build => ({
+    id: 'id',
+    number: 1,
+    status: 'PASSED',
+    commit: 'missing',
+    committedAt: Date.now(),
+    changeCount: 100,
+    ...build,
+  });
+  const makeResult = (ancestorBuilds: Build[]): AncestorBuildsQueryResult => ({
+    app: { build: { ancestorBuilds } },
+  });
+  it('returns a result in the first list of results', async () => {
+    const toFind = makeBuild({ number: 3, commit: 'exists' });
+    client.runQuery.mockReturnValue(makeResult([makeBuild(), makeBuild(), toFind]));
+
+    expect(await findAncestorBuildWithCommit({ client }, 1)).toEqual(toFind);
+    expect(client.runQuery).toHaveBeenCalledTimes(1);
+    expect(client.runQuery.mock.calls[0][1]).toMatchObject({ buildNumber: 1 });
+  });
+
+  it('passes skip and limit and recurse', async () => {
+    const toFind = makeBuild({ number: 3, commit: 'exists' });
+    client.runQuery
+      .mockReturnValueOnce(makeResult([makeBuild(), makeBuild()]))
+      .mockReturnValueOnce(makeResult([makeBuild(), toFind]));
+
+    expect(await findAncestorBuildWithCommit({ client }, 1, { page: 2, limit: 100 })).toEqual(
+      toFind
+    );
+    expect(client.runQuery).toHaveBeenCalledTimes(2);
+    expect(client.runQuery.mock.calls[0][1]).toMatchObject({ buildNumber: 1, skip: 0, limit: 2 });
+    expect(client.runQuery.mock.calls[1][1]).toMatchObject({ buildNumber: 1, skip: 2, limit: 2 });
+  });
+
+  it('bails out if we go past a limit', async () => {
+    client.runQuery
+      .mockReturnValueOnce(makeResult([makeBuild(), makeBuild()]))
+      .mockReturnValueOnce(makeResult([makeBuild(), makeBuild()]));
+
+    expect(await findAncestorBuildWithCommit({ client }, 1, { page: 2, limit: 3 })).toBeNull();
+    expect(client.runQuery).toHaveBeenCalledTimes(2);
+    expect(client.runQuery.mock.calls[0][1]).toMatchObject({ buildNumber: 1, skip: 0, limit: 2 });
+    expect(client.runQuery.mock.calls[1][1]).toMatchObject({ buildNumber: 1, skip: 2, limit: 1 });
   });
 });
--- bin-src/git/git.ts
+++ bin-src/git/git.ts
@@ -114,6 +114,38 @@ interface BaselineCommitsQueryResult {
   };
 }
 
+const AncestorBuildsQuery = gql`
+  query AncestorBuildsQuery($buildNumber: Int!, $skip: Int!, $limit: Int!) {
+    app {
+      build(number: $buildNumber) {
+        ancestorBuilds(skip: $skip, limit: $limit) {
+          id
+          number
+          status
+          commit
+          committedAt
+          changeCount
+        }
+      }
+    }
+  }
+`;
+
+export interface AncestorBuildsQueryResult {
+  app: {
+    build: {
+      ancestorBuilds: {
+        id: string;
+        number: number;
+        status: string;
+        commit: string;
+        committedAt: number;
+        changeCount: number;
+      }[];
+    };
+  };
+}
+
 export async function getVersion() {
   const result = await execGitCommand(`git --version`);
   return result.replace('git version ', '');
@@ -515,4 +547,48 @@ export async function discardChanges() {
 
 export async function getRepositoryRoot() {
   return execGitCommand(`git rev-parse --show-toplevel`);
+}
+
+/**
+ * If we have a build who's commit no longer exists in the repository (likely a rebase/force-pushed
+ * commit), search for an ancestor build which *does* have one.
+ *
+ * To do this we use the `Build.ancestorBuilds` API on the index, which will give us a set of builds
+ * in reverse "git-chronological" order. That is, if we pick the first build that the API gives us
+ * that has a commit, it is guaranteed to be the min number of builds "back" in Chromatic's history.
+ *
+ * The purpose here is to allow us to substitute a build with a known commit when doing TurboSnap.
+ *
+ * @param {int} number The build number to start searching from
+ * @param {object} options Page size and limit options
+ * @param {int} options.page How many builds to fetch each time
+ * @param {int} options.steps How far back to look
+ *
+ * @returns {Build | void} A build to be substituted
+ * */
+export async function findAncestorBuildWithCommit(
+  { client }: Pick<Context, 'client'>,
+  buildNumber: number,
+  { page = 10, limit = 80 } = {}
+): Promise<AncestorBuildsQueryResult['app']['build']['ancestorBuilds'][0] | void> {
+  let skip = 0;
+  while (skip < limit) {
+    // eslint-disable-next-line no-await-in-loop
+    const { app } = await client.runQuery(AncestorBuildsQuery, {
+      buildNumber,
+      skip,
+      limit: Math.min(page, limit - skip),
+    });
+
+    // eslint-disable-next-line no-await-in-loop
+    const results = await Promise.all(
+      app.build.ancestorBuilds.map(async (build) => [build, await commitExists(build.commit)])
+    );
+    const result = results.find(([build, exists]) => exists);
+
+    if (result) return result[0];
+
+    skip += page;
+  }
+  return null;
 }
