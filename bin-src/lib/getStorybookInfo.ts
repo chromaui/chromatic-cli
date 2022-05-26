6a8e35f52c9ea41165bbd76e6566b0d96fcb617d
thafryer <jarelfryer@gmail.com>
2022-05-26 11:48:50 -0500

    Add new test for when yarn search does not come up with anything

--- bin-src/lib/getStorybookInfo.ts
+++ bin-src/lib/getStorybookInfo.ts
@@ -2,43 +2,13 @@ import fs from 'fs-extra';
 import meow from 'meow';
 import { parseArgsStringToArgv } from 'string-argv';
 import semver from 'semver';
-import execa from 'execa';
 
 import noViewLayerPackage from '../ui/messages/errors/noViewLayerPackage';
 import { viewLayers } from './viewLayers';
 import { supportedAddons } from './supportedAddons';
 import { timeout, raceFulfilled } from './promises';
 import { Context } from '../types';
-
-export const getInstalledStorybookInfo = async () => {
-  const { all } = await execa.command('yarn list --pattern @storybook/ --json', {
-    env: {},
-    timeout: 10000,
-    all: true,
-    shell: true,
-  });
-
-  const { data } = JSON.parse(all as any);
-  const trees: { name: string; version: string }[] = data.trees.map((p: any) => {
-    const [, name, version] = p.name.match(/(.+)@(.+)/);
-    return { name, version };
-  });
-
-  const viewLayer = trees.find(({ name }) => viewLayers[name]);
-  const addons = trees.filter(({ name }) => supportedAddons[name]);
-
-  const result = {
-    viewLayer: viewLayers[viewLayer ? viewLayer.name : ''],
-    version: viewLayer ? viewLayer.version : undefined,
-    addons: addons.map((a) => ({
-      packageVersion: a.version,
-      packageName: a.name,
-      name: supportedAddons[a.name],
-    })),
-  };
-
-  return result;
-};
+import { getInstalledStorybookInfo } from './getInstalledStorybookPackages';
 
 const resolvePackageJson = (pkg: string) => {
   try {
