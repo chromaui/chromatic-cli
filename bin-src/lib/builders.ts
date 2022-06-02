244d0fc8a71ab7133109ad9bd5fc37f1cd5c9168
thafryer <jarelfryer@gmail.com>
2022-06-02 17:52:07 -0500

    Add new builder item"

--- bin-src/lib/builders.ts
+++ bin-src/lib/builders.ts
@@ -2,4 +2,5 @@ export const builders = {
   webpack4: '@storybook/builder-webpack4',
   webpack5: '@storybook/builder-webpack5',
   '@storybook/vite-builder': '@storybook/builder-vite',
+  '@storybook/builder-webpack5': '@storybook/builder-webpack5',
 } as Record<string, string>;
