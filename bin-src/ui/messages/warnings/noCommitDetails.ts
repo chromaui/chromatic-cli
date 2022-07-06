bc87b309a946a9dc72f4bc046884172c7af929ef
Gert Hengeveld <info@ghengeveld.nl>
2022-07-06 14:32:55 +0200

    Update warning to be more explicit when a custom GitHub workflow leads to an ephemeral commit getting recorded as build commit

--- bin-src/ui/messages/warnings/noCommitDetails.ts
+++ bin-src/ui/messages/warnings/noCommitDetails.ts
@@ -1,18 +1,34 @@
 import chalk from 'chalk';
 import { dedent } from 'ts-dedent';
 
-import { warning } from '../../components/icons';
+import { info, warning } from '../../components/icons';
+import link from '../../components/link';
 
-export default (sha: string, envVar?: string) =>
-  envVar
-    ? dedent(chalk`
-      ${warning} {bold Commit ${sha.substr(0, 7)} does not exist}
-      We tried to retrieve the commit details but couldn't find it in your Git history.
-      Check your {bold ${envVar}} environment variable.
-      Continuing with just the commit hash.
-    `)
-    : dedent(chalk`
-      ${warning} {bold Commit ${sha.substr(0, 7)} does not exist}
-      We tried to retrieve the commit details but couldn't find it in your Git history.
-      Continuing with just the commit hash.
+type BranchRef = { ref: string; sha: string; env: string };
+type CommitRef = { ref?: never; sha: string; env?: string };
+
+export default ({ ref, sha, env }: BranchRef | CommitRef) => {
+  if (ref) {
+    return dedent(chalk`
+      ${warning} {bold Branch '${ref}' does not exist}
+      We tried to retrieve its latest commit but couldn't find it in your Git history.
+      Falling back to ${sha.slice(0, 7)}, but commit details (date, author) will be missing.
+      Pull request status updates likely won't work properly.
+      Please use our official GitHub Action or forward the pull_request event info to us.
+      ${info} Read more at ${link('https://www.chromatic.com/docs/github-actions')}
     `);
+  }
+  if (env) {
+    return dedent(chalk`
+      ${warning} {bold Commit ${sha.slice(0, 7)}} does not exist}
+      We tried to retrieve the commit details but couldn't find it in your Git history.
+      Check your {bold ${env}} environment variable.
+      Using it anyway, but commit details (date, author) will be missing.
+    `);
+  }
+  return dedent(chalk`
+    ${warning} {bold Commit ${sha.slice(0, 7)} does not exist}
+    We tried to retrieve the commit details but couldn't find it in your Git history.
+    Using it anyway, but commit details (date, author) will be missing.
+  `);
+};
