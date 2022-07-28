c16c2da7b236d7d4a8088529abeec687b175acff
Reuben Ellis <ethriel3695@gmail.com>
2022-07-28 09:08:17 -0600

    Update spacing with editorconfig

--- bin-src/trim-stats-file.test.ts
+++ bin-src/trim-stats-file.test.ts
@@ -6,16 +6,16 @@ import { readStatsFile } from './trim-stats-file';
 import * as trimmedFile from './__mocks__/previewStatsJson/preview-stats.trimmed.json';
 
 mockfs({
-    './storybook-static/preview-stats.json': JSON.stringify(trimmedFile),
+  './storybook-static/preview-stats.json': JSON.stringify(trimmedFile),
 })
 
 afterEach(() => {
-    mockfs.restore();
+  mockfs.restore();
 })
 
 describe('Trim Stats File', () => {
-    it('readStatsFile returns expected output', async () => {
-        const result = await readStatsFile('./storybook-static/preview-stats.json');
-        expect(result.modules.some(({ id }) => id === './bin-src/ui/components/icons.stories.ts')).toBe(true);
-    });
+  it('readStatsFile returns expected output', async () => {
+    const result = await readStatsFile('./storybook-static/preview-stats.json');
+    expect(result.modules.some(({ id }) => id === './bin-src/ui/components/icons.stories.ts')).toBe(true);
+  });
 });
