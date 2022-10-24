32bd8185f45c8c3431fd3e98b61d27a5e433489c
Steven Kitterman <steven.kitterman@gmail.com>
2022-10-24 16:11:40 -0700

    Test added that bailing did not happen

--- bin-src/lib/getDependentStoryFiles.test.ts
+++ bin-src/lib/getDependentStoryFiles.test.ts
@@ -382,6 +382,7 @@ describe('getDependentStoryFiles', () => {
     const rawContext = getContext();
     const ctx = {
       ...rawContext,
+      // signifying the package.json file had dependency changes
       git: { ...rawContext.git, changedPackageManifests: ['src/package.json'] },
     };
     const res = await getDependentStoryFiles(ctx, { modules }, statsPath, changedFiles);
@@ -392,6 +393,28 @@ describe('getDependentStoryFiles', () => {
     expect(ctx.log.warn).toHaveBeenCalledWith(
       expect.stringContaining(chalk`Found a dependency change in {bold src/package.json}`)
     );
+  });
+
+  it('does not bail when package.json change has no dependency changes', async () => {
+    const changedFiles = ['src/foo.stories.js', 'src/package.json'];
+    const modules = [
+      {
+        id: './src/foo.stories.js',
+        name: './src/foo.stories.js',
+        reasons: [{ moduleName: CSF_GLOB }],
+      },
+      {
+        id: CSF_GLOB,
+        name: CSF_GLOB,
+        reasons: [{ moduleName: './.storybook/generated-stories-entry.js' }],
+      },
+    ];
+    const ctx = getContext();
+    const res = await getDependentStoryFiles(ctx, { modules }, statsPath, changedFiles);
+    expect(ctx.turboSnap.bailReason).toBeUndefined();
+    expect(res).toEqual({
+      './src/foo.stories.js': ['src/foo.stories.js'],
+    });
   });
 
   it('bails on changed Storybook config file', async () => {
