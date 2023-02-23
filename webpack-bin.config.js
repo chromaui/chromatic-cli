12b8f45fffe4891c8624ab046b94a40d53d98766
Reuben Ellis <ethriel3695@gmail.com>
2023-02-23 16:55:57 -0700

    Update webpack node targets

--- webpack-bin.config.js
+++ webpack-bin.config.js
@@ -5,7 +5,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  target: 'node12',
+  target: 'node16',
   entry: './bin-src/register.js',
   externalsPresets: { node: true },
   // devtool: 'source-map',
