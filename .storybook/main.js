846e1ae7e27f5058e91ab7fe95ce4f16257b53b5
Jono Kolnik <1164060+JonathanKolnik@users.noreply.github.com>
2023-04-05 09:32:31 -0400

    upgrade to sb v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,27 @@
 module.exports = {
   stories: process.env.SMOKE_TEST ? ['../test-stories/*.stories.*'] : ['../bin-src/**/*.stories.*'],
-  addons: ['@storybook/addon-viewport'],
+  addons: ['@storybook/addon-viewport', '@storybook/addon-mdx-gfm'],
   features: {
-    postcss: false,
+    postcss: false
   },
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config, {
+    configType
+  }) => {
     // eslint-disable-next-line no-param-reassign
     config.resolve = {
       ...config.resolve,
       fallback: {
         ...config.resolve.fallback,
-        os: require.resolve('os-browserify/browser'),
-      },
+        os: require.resolve('os-browserify/browser')
+      }
     };
-
     return config;
   },
+  docs: {
+    autodocs: true
+  }
 };
