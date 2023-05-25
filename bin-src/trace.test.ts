8bddbc1fa1c52c080e802484984ea9b99af0a036
Reuben Ellis <ethriel3695@gmail.com>
2023-05-25 15:29:17 -0500

    Made the test more robust

--- bin-src/trace.test.ts
+++ bin-src/trace.test.ts
@@ -1,17 +1,16 @@
 import chalk from 'chalk';
 import { execSync } from 'child_process';
 
-const scriptCommand = `$ ./bin/main.cjs trace ./bin-src/ui/messages/errors/invalidRepositorySlug.ts`;
-const tracedBegin = `Traced ${chalk`{bold 1 changed file}`} to ${chalk`{bold 1 affected story file}`}`;
-const moduleName = `bin-src/ui/messages/errors/${chalk`{bold invalidRepositorySlug.stories.ts}`}`;
-const storyIndex = `[story index]`;
-const tracedEnd = `Set ${chalk`{bold --mode (-m)}`} to ${chalk`{bold 'expanded'}`} to reveal underlying modules.`;
+const scriptCommand = `$ ./bin/main.cjs trace ./bin-src/ui/messages/errors/invalidReportPath.ts`;
+const tracedBegin = `Traced 1 changed file to 1 affected story file`;
+const moduleName = `bin-src/ui/messages/errors/invalidReportPath.stories.ts`;
 
 const rootDirNote = `The root directory of your project:`;
 
 describe('Test trace script from package.json', () => {
   it('returns the default output successfully', () => {
-    const scriptName = 'chromatic trace ./bin-src/ui/messages/errors/invalidRepositorySlug.ts';
+    const scriptName =
+      'chromatic trace ./bin-src/ui/messages/errors/invalidReportPath.ts -s bin-src/__mocks__/previewStatsJson/preview-stats.trimmed.json';
 
     // Execute the script as a child process
     const output = execSync(`yarn ${scriptName}`).toString().trim();
@@ -20,15 +19,13 @@ describe('Test trace script from package.json', () => {
     expect(output).toContain(scriptCommand);
     expect(output).toContain(tracedBegin);
     expect(output).toContain(moduleName);
-    expect(output).toContain(storyIndex);
-    expect(output).toContain(tracedEnd);
 
     // Verify that the output does not contain the expanded output
     expect(output).not.toContain(rootDirNote);
   });
   it('outputs directory info when -m expanded is passed', () => {
     const scriptName =
-      'chromatic trace ./bin-src/ui/messages/errors/invalidRepositorySlug.ts -m expanded';
+      'chromatic trace ./bin-src/ui/messages/errors/invalidReportPath.ts -s bin-src/__mocks__/previewStatsJson/preview-stats.trimmed.json -m expanded';
 
     // Execute the script as a child process
     const output = execSync(`yarn ${scriptName}`).toString().trim();
@@ -39,8 +36,8 @@ describe('Test trace script from package.json', () => {
   1. Make sure you have the correct root path, base path, and storybook path.\n
   2. Make sure you have the correct storybook config file.\n
   3. Make sure you have the correct storybook config file path.\nYou can either set the flags storybook-base-dir or storybook-config-dir to help TurboSnap find the correct storybook config file.\n`;
-    const expandedStoryModule = `bin-src/ui/messages/errors/invalidRepositorySlug.stories.ts`;
-    const expandedFileModule = `bin-src/ui/messages/errors/invalidRepositorySlug.ts`;
+    const expandedStoryModule = `bin-src/ui/messages/errors/invalidReportPath.stories.ts`;
+    const expandedFileModule = `bin-src/ui/messages/errors/invalidReportPath.ts`;
 
     // Add your assertions based on the expected output or behavior of the script
     expect(output).toContain(rootDirNote);
@@ -52,12 +49,12 @@ describe('Test trace script from package.json', () => {
   });
   it('outputs untraced info when --mode expanded is passed with -u and an untraced file', () => {
     const scriptName =
-      'chromatic trace ./bin-src/ui/messages/errors/invalidRepositorySlug.ts -m expanded -u ./bin-src/ui/messages/errors/invalidRepositorySlug.ts';
+      'chromatic trace ./bin-src/ui/messages/errors/invalidReportPath.ts -s bin-src/__mocks__/previewStatsJson/preview-stats.trimmed.json -m expanded -u ./bin-src/ui/messages/errors/invalidReportPath.ts';
 
     // Execute the script as a child process
     const output = execSync(`yarn ${scriptName}`).toString().trim();
 
-    const untracedFile = `./bin-src/ui/messages/errors/invalidRepositorySlug.ts`;
+    const untracedFile = `./bin-src/ui/messages/errors/invalidReportPath.ts`;
     const untracedFileNote = `We detected some untraced files`;
 
     expect(output).toContain(untracedFileNote);
