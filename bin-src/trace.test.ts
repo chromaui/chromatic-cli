47c34f23061a73e234c6ff2c19d09e54e273dc56
Reuben Ellis <ethriel3695@gmail.com>
2023-05-25 11:22:32 -0500

    Add better logging for tracing dependencies

--- bin-src/trace.test.ts
+++ bin-src/trace.test.ts
@@ -1,18 +1,66 @@
-import mockfs from 'mock-fs';
+import chalk from 'chalk';
+import { execSync } from 'child_process';
 
-// eslint-disable-next-line jest/no-mocks-import
-import * as trimmedFile from './__mocks__/previewStatsJson/preview-stats.trimmed.json';
+const scriptCommand = `$ ./bin/main.cjs trace ./bin-src/ui/messages/errors/invalidRepositorySlug.ts`;
+const tracedBegin = `Traced ${chalk`{bold 1 changed file}`} to ${chalk`{bold 1 affected story file}`}`;
+const moduleName = `bin-src/ui/messages/errors/${chalk`{bold invalidRepositorySlug.stories.ts}`}`;
+const storyIndex = `[story index]`;
+const tracedEnd = `Set ${chalk`{bold --mode (-m)}`} to ${chalk`{bold 'expanded'}`} to reveal underlying modules.`;
 
-mockfs({
-  './storybook-static/preview-stats.json': JSON.stringify(trimmedFile),
-});
+const rootDirNote = `The root directory of your project:`;
 
-afterEach(() => {
-  mockfs.restore();
-});
+describe('Test trace script from package.json', () => {
+  it('returns the default output successfully', () => {
+    const scriptName = 'chromatic trace ./bin-src/ui/messages/errors/invalidRepositorySlug.ts';
 
-describe('Trace', () => {
-  it('acts as a placeholder for a trace test', async () => {
-    expect(true).toBe(true);
+    // Execute the script as a child process
+    const output = execSync(`yarn ${scriptName}`).toString().trim();
+
+    // Add your assertions based on the expected output or behavior of the script
+    expect(output).toContain(scriptCommand);
+    expect(output).toContain(tracedBegin);
+    expect(output).toContain(moduleName);
+    expect(output).toContain(storyIndex);
+    expect(output).toContain(tracedEnd);
+
+    // Verify that the output does not contain the expanded output
+    expect(output).not.toContain(rootDirNote);
+  });
+  it('outputs directory info when -m expanded is passed', () => {
+    const scriptName =
+      'chromatic trace ./bin-src/ui/messages/errors/invalidRepositorySlug.ts -m expanded';
+
+    // Execute the script as a child process
+    const output = execSync(`yarn ${scriptName}`).toString().trim();
+
+    const baseDirNote = `The base directory (The relative path from the root to the storybook config root):`;
+    const storybookDirNote = `The storybook directory (The directory can either be at the root or in a sub-directory):`;
+    const traceSuggestions = `If you are having trouble with tracing, please check the following:\n
+  1. Make sure you have the correct root path, base path, and storybook path.\n
+  2. Make sure you have the correct storybook config file.\n
+  3. Make sure you have the correct storybook config file path.\nYou can either set the flags storybook-base-dir or storybook-config-dir to help TurboSnap find the correct storybook config file.\n`;
+    const expandedStoryModule = `bin-src/ui/messages/errors/invalidRepositorySlug.stories.ts`;
+    const expandedFileModule = `bin-src/ui/messages/errors/invalidRepositorySlug.ts`;
+
+    // Add your assertions based on the expected output or behavior of the script
+    expect(output).toContain(rootDirNote);
+    expect(output).toContain(baseDirNote);
+    expect(output).toContain(storybookDirNote);
+    expect(output).toContain(traceSuggestions);
+    expect(output).toContain(expandedStoryModule);
+    expect(output).toContain(expandedFileModule);
+  });
+  it('outputs untraced info when --mode expanded is passed with -u and an untraced file', () => {
+    const scriptName =
+      'chromatic trace ./bin-src/ui/messages/errors/invalidRepositorySlug.ts -m expanded -u ./bin-src/ui/messages/errors/invalidRepositorySlug.ts';
+
+    // Execute the script as a child process
+    const output = execSync(`yarn ${scriptName}`).toString().trim();
+
+    const untracedFile = `./bin-src/ui/messages/errors/invalidRepositorySlug.ts`;
+    const untracedFileNote = `We detected some untraced files`;
+
+    expect(output).toContain(untracedFileNote);
+    expect(output).toContain(untracedFile);
   });
 });
--- bin-src/ui/messages/info/tracedAffectedFiles.ts
+++ bin-src/ui/messages/info/tracedAffectedFiles.ts
@@ -38,7 +38,30 @@ export default (
 
   const changed = pluralize('changed files', changedFiles.length, true);
   const affected = pluralize('affected story files', Object.keys(affectedModules).length, true);
-  const directoryDebug = chalk`Root Directory: ${ctx.turboSnap.rootPath}\n\n Base Directory: ${ctx.turboSnap.baseDir}\n\n Storybook Directory: ${ctx.turboSnap.storybookDir}\n\n ${ctx.turboSnap.tracedPaths}\n\n`;
+  const bailReason = ctx.turboSnap.bailReason
+    ? `${chalk.magenta('Bail Reason:')} ${ctx.turboSnap.bailReason}\n\n`
+    : '';
+  const rootPath = `${chalk.magenta('The root directory of your project:')} ${
+    ctx.turboSnap.rootPath
+  }\n\n`;
+  const basePath = `${chalk.magenta(
+    'The base directory (The relative path from the root to the storybook config root):'
+  )} ${ctx.turboSnap.baseDir}\n\n`;
+  const storybookPath = `${chalk.magenta(
+    'The storybook directory (The directory can either be at the root or in a sub-directory):'
+  )} ${ctx.turboSnap.storybookDir}\n\n`;
+  const untracedNotice =
+    ctx.untracedFiles.length > 0
+      ? `${chalk.magenta(
+          `We detected some untraced files, this may affect your traced changes as 
+    the untraced flag instructs TurboSnap to not trace dependencies for the files:`
+        )} \n  ${ctx.untracedFiles.join(',')}\n\n\n`
+      : '';
+  const directoryDebug = `${rootPath}${basePath}${storybookPath}${bailReason}${untracedNotice}If you are having trouble with tracing, please check the following:\n
+  1. Make sure you have the correct root path, base path, and storybook path.\n
+  2. Make sure you have the correct storybook config file.\n
+  3. Make sure you have the correct storybook config file path.\nYou can either set the flags storybook-base-dir or storybook-config-dir to help TurboSnap find the correct storybook config file.\n
+  `;
   const summary = chalk`${
     ctx.options.traceChanged === 'expanded' ? directoryDebug : ''
   } ${info} Traced {bold ${changed}} to {bold ${affected}}`;
