c5499e6498ca954a5f2bfc6b0fe72c057bb3a520
Tom Coleman <tom@chromatic.com>
2023-06-05 21:49:19 +1000

    Use tsup for bundling

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
-  entry: ['node-src/index.ts'],
-  outDir: 'node',
+  entry: {
+    action: 'action-src/register.js',
+    bin: 'bin-src/register.js',
+    node: 'node-src/index.ts',
+  },
   splitting: false,
   minify: !options.watch,
   format: ['cjs'],
   dts: {
+    entry: ['node-src/index.ts'],
     resolve: true,
   },
   treeshake: true,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
   platform: 'node',
   target: 'node16',
