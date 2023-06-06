7223c7b1a7f5ab400b011d13aabbf4db08fbc877
Tom Coleman <tom@chromatic.com>
2023-06-06 11:13:50 +1000

    Apply @ghengeveld's suggestions

--- node-src/lib/tasks.ts
+++ node-src/lib/tasks.ts
@@ -16,7 +16,7 @@ export const createTask = ({ title, steps, ...config }): Listr.ListrTask<Context
       await step(ctx, task);
     }
 
-    ctx.options.onTaskComplete?.(ctx);
+    ctx.options.onTaskComplete?.({ ...ctx });
   },
   ...config,
 });
