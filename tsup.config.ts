2989d0dcba7dd920b1a679327fcacf09493aa034
Tom Coleman <tom@chromatic.com>
2023-06-07 12:08:18 +1000

    Use a separate tsup config for the action

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,21 +1,34 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig((options) => ({
-  entry: {
-    action: 'action-src/register.js',
-    bin: 'bin-src/register.js',
-    node: 'node-src/index.ts',
+export default defineConfig((options) => [
+  {
+    entry: {
+      bin: 'bin-src/register.js',
+      node: 'node-src/index.ts',
+    },
+    splitting: true,
+    minify: !options.watch,
+    format: ['cjs'],
+    dts: {
+      entry: ['node-src/index.ts'],
+      resolve: true,
+    },
+    treeshake: true,
+    sourcemap: false,
+    clean: true,
+    platform: 'node',
+    target: 'node16',
   },
-  splitting: false,
-  minify: !options.watch,
-  format: ['cjs'],
-  dts: {
-    entry: ['node-src/index.ts'],
-    resolve: true,
+  {
+    entry: ['action-src/register.js'],
+    outDir: 'action',
+    splitting: false,
+    minify: !options.watch,
+    format: ['cjs'],
+    treeshake: true,
+    sourcemap: false,
+    clean: true,
+    platform: 'node',
+    target: 'node16',
   },
-  treeshake: true,
-  sourcemap: false,
-  clean: true,
-  platform: 'node',
-  target: 'node16',
-}));
+]);
