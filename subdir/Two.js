fea5a859946d8e16592716ff2cb80d9271fec113
Matthew Weeks <weeksling@gmail.com>
2023-06-22 09:37:07 -0400

    now add a visual change

--- subdir/Two.js
+++ subdir/Two.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import { One } from './One';
 
-export const Two = ({ children }) => <One>Two {children}</One>;
+export const Two = ({ children }) => <One style={{margin: "5px", color: "red"}}>Two {children}</One>;
