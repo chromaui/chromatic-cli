ba8887e32c9af0a51bdda4f51e50a89a9f587f39
Tom Coleman <tom@chromatic.com>
2023-08-10 15:08:50 +1000

    Update the warning to mention VT addon

--- node-src/ui/messages/warnings/noCommitDetails.ts
+++ node-src/ui/messages/warnings/noCommitDetails.ts
@@ -13,6 +13,7 @@ export default ({ ref, sha, env }: BranchRef | CommitRef) => {
       ${warning} {bold Branch '${ref}' does not exist}
       We tried to retrieve its latest commit but couldn't find it in your Git history.
       Falling back to ${sha.slice(0, 7)}, but commit details (date, author) will be missing.
+      We will not be able to retain baselines from builds created by the Visual Tests addon.
       Pull request status updates likely won't work properly.
       Please use our official GitHub Action or forward the pull_request event info to us.
       ${info} Read more at ${link('https://www.chromatic.com/docs/github-actions')}
@@ -24,11 +25,13 @@ export default ({ ref, sha, env }: BranchRef | CommitRef) => {
       We tried to retrieve the commit details but couldn't find it in your Git history.
       Check your {bold ${env}} environment variable.
       Using it anyway, but commit details (date, author) will be missing.
+      We will not be able to retain baselines from builds created by the Visual Tests addon.
     `);
   }
   return dedent(chalk`
     ${warning} {bold Commit ${sha.slice(0, 7)} does not exist}
     We tried to retrieve the commit details but couldn't find it in your Git history.
     Using it anyway, but commit details (date, author) will be missing.
+    We will not be able to retain baselines from builds created by the Visual Tests addon.
   `);
 };
