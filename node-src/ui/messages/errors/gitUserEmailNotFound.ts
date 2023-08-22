a7d2ed4d0859c36edbd0556bc63751561b8988d4
Tom Coleman <tom@chromatic.com>
2023-08-22 10:24:24 +1000

    Fix up text a little

--- node-src/ui/messages/errors/gitUserEmailNotFound.ts
+++ node-src/ui/messages/errors/gitUserEmailNotFound.ts
@@ -11,12 +11,12 @@ export default () =>
   `${dedent(chalk`
     ${error} {bold Failed to find the current git user's email}
     We were unable to find your git email so this local build
-    will not belong to you and will not affect your future baselines
-    (read more: ${link(localBuildsDocsLink)}).
+    will not belong to you and will not affect your future baselines.
+    Read more: ${link(localBuildsDocsLink)}
     
-    In order to associate your local changes with later CI builds, you need to configure
-    git with the email address you'll commit with.
-    You can do this with \`git config --global user.email YOUR_EMAIL\`
+    In order to associate your local changes with later CI builds, you
+    need to configure git with the email address you'll commit with.
+    You can do this with \`git config --global user.email YOUR_EMAIL\`.
 
     Once you've done so, please run this build again.
   `)}`;
