8ec351823feb453910a89bd204d8676a39ec9681
Tom Coleman <tom@chromatic.com>
2023-09-12 12:25:05 +1000

    Fix incorrect message

--- node-src/ui/messages/errors/missingConfigurationFile.ts
+++ node-src/ui/messages/errors/missingConfigurationFile.ts
@@ -5,7 +5,7 @@ import { error } from '../../components/icons';
 
 export const missingConfigurationFile = (configFile: string) =>
   dedent(chalk`
-    ${error} Configuration file {bold configFile} could not be found.
+    ${error} Configuration file {bold ${configFile}} could not be found.
 
     Check the {bold --config-file} flag of the CLI.
   `);
