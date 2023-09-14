3fe6a8a2f99429ee50c400052c9c91052de20cfb
Tom Coleman <tom@chromatic.com>
2023-09-14 12:15:11 +1000

    Fix invalid message

--- node-src/ui/messages/errors/invalidConfigurationFile.ts
+++ node-src/ui/messages/errors/invalidConfigurationFile.ts
@@ -8,7 +8,7 @@ export const invalidConfigurationFile = (configFile: string, err: ZodError) => {
   const { formErrors, fieldErrors } = err.flatten();
 
   return dedent(chalk`
-    ${error} Configuration file {bold configFile} was invalid, please check the allowed keys.
+    ${error} Configuration file {bold ${configFile}} was invalid, please check the allowed keys.
     ${
       formErrors.length
         ? `\n${formErrors.map((msg) => chalk`- {bold ${msg}}`).join('\n    ')}\n\n`
