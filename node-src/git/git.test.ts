a8e78e852fe5408e4a477331b34f841ad0987a5a
Gert Hengeveld <info@ghengeveld.nl>
2023-10-09 14:02:48 +0200

    Add tests

--- node-src/git/git.test.ts
+++ node-src/git/git.test.ts
@@ -1,11 +1,48 @@
 import { execaCommand } from 'execa';
 import { describe, expect, it, vi } from 'vitest';
 
-import { getSlug } from './git';
+import { getCommit, getSlug } from './git';
 
 vi.mock('execa');
 
 const command = vi.mocked(execaCommand);
+
+describe('getCommit', () => {
+  it('parses log output', async () => {
+    command.mockImplementation(
+      () =>
+        Promise.resolve({
+          all: `19b6c9c5b3d34d9fc55627fcaf8a85bd5d5e5b2a ## 1696588814 ## info@ghengeveld.nl ## Gert Hengeveld`,
+        }) as any
+    );
+    expect(await getCommit()).toEqual({
+      commit: '19b6c9c5b3d34d9fc55627fcaf8a85bd5d5e5b2a',
+      committedAt: 1696588814 * 1000,
+      committerEmail: 'info@ghengeveld.nl',
+      committerName: 'Gert Hengeveld',
+    });
+  });
+
+  it('ignores gpg signature information', async () => {
+    command.mockImplementation(
+      () =>
+        Promise.resolve({
+          all: `
+gpg: Signature made Fri Oct  6 12:40:14 2023 CEST
+gpg:                using RSA key 4AEE18F83AFDEB23
+gpg: Can't check signature: No public key
+19b6c9c5b3d34d9fc55627fcaf8a85bd5d5e5b2a ## 1696588814 ## info@ghengeveld.nl ## Gert Hengeveld
+          `.trim(),
+        }) as any
+    );
+    expect(await getCommit()).toEqual({
+      commit: '19b6c9c5b3d34d9fc55627fcaf8a85bd5d5e5b2a',
+      committedAt: 1696588814 * 1000,
+      committerEmail: 'info@ghengeveld.nl',
+      committerName: 'Gert Hengeveld',
+    });
+  });
+});
 
 describe('getSlug', () => {
   it('returns the slug portion of the git url', async () => {
