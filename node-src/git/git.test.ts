e6bcce976600c5faeb2fe3efd0e1734343e6622d
Gert Hengeveld <info@ghengeveld.nl>
2023-10-09 14:07:52 +0200

    Add tests for hasPreviousCommit

--- node-src/git/git.test.ts
+++ node-src/git/git.test.ts
@@ -1,7 +1,7 @@
 import { execaCommand } from 'execa';
 import { describe, expect, it, vi } from 'vitest';
 
-import { getCommit, getSlug } from './git';
+import { getCommit, getSlug, hasPreviousCommit } from './git';
 
 vi.mock('execa');
 
@@ -60,5 +60,34 @@ describe('getSlug', () => {
       () => Promise.resolve({ all: 'https://gitlab.com/foo/bar.baz.git' }) as any
     );
     expect(await getSlug()).toBe('foo/bar.baz');
+  });
+});
+
+describe('hasPreviousCommit', () => {
+  it('returns true if a commit is found', async () => {
+    command.mockImplementation(
+      () => Promise.resolve({ all: `19b6c9c5b3d34d9fc55627fcaf8a85bd5d5e5b2a` }) as any
+    );
+    expect(await hasPreviousCommit()).toEqual(true);
+  });
+
+  it('returns false if no commit is found', async () => {
+    command.mockImplementation(() => Promise.resolve({ all: `` }) as any);
+    expect(await hasPreviousCommit()).toEqual(false);
+  });
+
+  it('ignores gpg signature information', async () => {
+    command.mockImplementation(
+      () =>
+        Promise.resolve({
+          all: `
+gpg: Signature made Fri Oct  6 12:40:14 2023 CEST
+gpg:                using RSA key 4AEE18F83AFDEB23
+gpg: Can't check signature: No public key
+19b6c9c5b3d34d9fc55627fcaf8a85bd5d5e5b2a
+          `.trim(),
+        }) as any
+    );
+    expect(await hasPreviousCommit()).toEqual(true);
   });
 });
