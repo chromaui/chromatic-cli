97b966ce8756c4e376fd2b609bbf30d9d5f9fd36
Gert Hengeveld <info@ghengeveld.nl>
2023-10-21 20:19:53 +0200

    Add viewport meta tag

--- node-src/ui/html/metadata.html.ts
+++ node-src/ui/html/metadata.html.ts
@@ -12,6 +12,7 @@ export default (
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Build ${announcedBuild.number} metadata files</title>
     <style>
       body {
