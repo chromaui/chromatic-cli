d2d1dbba407d053a76e3b4d3d217dd6848cb6c4c
Andrew Ortwein <andrewortwein@icloud.com>
2024-02-20 16:00:34 -0500

    Comment out checkStorybookBaseDir to see if smoke tests pass

--- node-src/tasks/upload.ts
+++ node-src/tasks/upload.ts
@@ -30,7 +30,7 @@ import { findChangedDependencies } from '../lib/findChangedDependencies';
 import { uploadBuild } from '../lib/upload';
 import { getFileHashes } from '../lib/getFileHashes';
 import { waitForSentinel } from '../lib/waitForSentinel';
-import { checkStorybookBaseDir } from '../lib/checkStorybookBaseDir';
+// import { checkStorybookBaseDir } from '../lib/checkStorybookBaseDir';
 
 interface PathSpec {
   pathname: string;
@@ -144,7 +144,7 @@ export const traceChangedFiles = async (ctx: Context, task: Task) => {
 
     const stats = await readStatsFile(statsPath);
 
-    await checkStorybookBaseDir(ctx, stats);
+    // await checkStorybookBaseDir(ctx, stats);
 
     const onlyStoryFiles = await getDependentStoryFiles(
       ctx,
