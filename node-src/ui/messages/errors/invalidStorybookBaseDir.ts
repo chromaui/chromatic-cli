5f465d9174249b2d3914b93bf01e891367805f98
Andrew Ortwein <andrewortwein@icloud.com>
2024-02-21 16:29:31 -0500

    Add turbosnap-helper to error message

--- node-src/ui/messages/errors/invalidStorybookBaseDir.ts
+++ node-src/ui/messages/errors/invalidStorybookBaseDir.ts
@@ -8,4 +8,5 @@ export const invalidStorybookBaseDir = () =>
     ${error} TurboSnap disabled until base directory is set correctly
     The base directory allows TurboSnap to trace files.
     Set the {bold --storybook-base-dir} option as the relative path from the repository root to the Storybook project root.
+    Run {bold @chromatic-com/turbosnap-helper} to get your base directory value.
   `);
