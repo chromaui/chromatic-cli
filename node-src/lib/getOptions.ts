a7def3a3566592b2753d7be110ca07f9507d6084
Sander.Schutten@athora.nl <Sander.Schutten@athora.nl>
2024-02-22 11:05:21 +0100

    feat: skipUpdateCheck option

--- node-src/lib/getOptions.ts
+++ node-src/lib/getOptions.ts
@@ -69,6 +69,7 @@ export default function getOptions({
     forceRebuild: undefined,
     junitReport: undefined,
     zip: undefined,
+    skipUpdateCheck: undefined,
 
     ignoreLastBuildOnBranch: undefined,
     preserveMissingSpecs: undefined,
