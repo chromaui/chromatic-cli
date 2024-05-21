ac4c6bd84ec1d35ca5afc8a39883fd0e4e99da6f
Reuben Ellis <ethriel3695@gmail.com>
2024-05-20 18:52:03 -0600

    Update verify statement

--- node-src/ui/tasks/verify.ts
+++ node-src/ui/tasks/verify.ts
@@ -38,9 +38,11 @@ export const runOnlyFiles = (ctx: Context) => ({
 export const runOnlyNames = (ctx: Context) => ({
   status: 'pending',
   title: 'Starting partial build',
-  output: `Snapshots will be limited to stories matching ${ctx.options.onlyStoryNames
-    .map((v) => `'${v}'`)
-    .join(', ')}`,
+  output: ctx.options.onlyStoryNames
+    ? `Snapshots will be limited to stories matching ${ctx.options.onlyStoryNames
+        .map((v) => `'${v}'`)
+        .join(', ')}`
+    : `Snapshots will be limited to ${ctx.onlyStoryFiles?.length} story files affected by recent changes`,
 });
 
 export const awaitingUpgrades = (ctx: Context, upgradeBuilds: { completedAt?: number }[]) => {
