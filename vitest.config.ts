c8cf4f38489c36b75c263039eaa0ab682154b794
Cody Kaup <dbkaup@gmail.com>
2024-09-10 10:02:52 -0500

    Exclude vitest config from coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,12 @@
 /* eslint-disable import/no-unresolved */
-import { configDefaults, defineConfig } from 'vitest/config';
+import { configDefaults, coverageConfigDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude, '**/getParentCommits.test.ts'],
+    coverage: {
+      provider: 'v8',
+      exclude: ['vitest.no-threads.config.ts', ...coverageConfigDefaults.exclude],
+    },
   },
 });
