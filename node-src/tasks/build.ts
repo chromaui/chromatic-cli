ba8c70f10be9e44356987fed39b4716ef22bb383
Maxie <maxcorbin@users.noreply.github.com>
2024-09-13 04:03:00 -0700

    Revert "Use --stats-json flag for SB 8.0.0+"

--- node-src/tasks/build.ts
+++ node-src/tasks/build.ts
@@ -27,11 +27,6 @@ export const setSourceDir = async (ctx: Context) => {
   }
 };
 
-// Storybook 8.0.0 deprecated --webpack-stats-json in favor of --stats-json.
-const webpackStatsFlag = (version: string) => {
-  return semver.gte(semver.coerce(version), '8.0.0') ? '--stats-json' : '--webpack-stats-json';
-};
-
 export const setBuildCommand = async (ctx: Context) => {
   const webpackStatsSupported =
     ctx.storybook && ctx.storybook.version
@@ -44,9 +39,7 @@ export const setBuildCommand = async (ctx: Context) => {
 
   const buildCommandOptions = [
     `--output-dir=${ctx.sourceDir}`,
-    ctx.git.changedFiles &&
-      webpackStatsSupported &&
-      `${webpackStatsFlag(ctx.storybook.version)}=${ctx.sourceDir}`,
+    ctx.git.changedFiles && webpackStatsSupported && `--webpack-stats-json=${ctx.sourceDir}`,
   ].filter(Boolean);
 
   ctx.buildCommand = await (isE2EBuild(ctx.options)
