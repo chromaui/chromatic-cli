794c543b0a50b99494270476eff7c442b541c99e
Cody Kaup <dbkaup@gmail.com>
2024-09-30 09:54:32 -0500

    Enable @typescript-eslint/no-floating-promises

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -71,8 +71,7 @@ export default [
       // TODO: make this an error when we have time to deal with it
       '@typescript-eslint/no-explicit-any': 'off',
       //   '@typescript-eslint/no-explicit-any': 'warn',
-      // TODO: make this an error when we have time to deal with it
-      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-floating-promises': 'error',
     },
   },
   {
