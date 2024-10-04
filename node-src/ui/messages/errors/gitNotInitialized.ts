b902a0029b4dcb6eb3d73cd40d139f55841d8776
Cody Kaup <dbkaup@gmail.com>
2024-10-04 15:40:02 -0500

    Show steps for initializing a new Git repo

--- node-src/ui/messages/errors/gitNotInitialized.ts
+++ node-src/ui/messages/errors/gitNotInitialized.ts
@@ -7,4 +7,13 @@ export default ({ command }: { command: string }) =>
   dedent(chalk`
     ${error} {bold Unable to execute command}: ${command}
     Chromatic only works from inside a Git repository.
+
+    You can initialize a new Git repository with \`git init\`.
+
+    You will also need a single commit in order to run a build. To do that:
+
+    - Add a file (or multiple files) with \`git add <FILE_PATH(S)>\`
+    - Commit the file(s) with \`git commit --message="<MESSAGE>"\`
+
+    Once you've done so, please run this build again.
   `);
