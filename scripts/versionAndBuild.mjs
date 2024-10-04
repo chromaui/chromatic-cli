0542e1fe732ced869f8a9d51c3b8bca21970d5d0
Cody Kaup <dbkaup@gmail.com>
2024-10-04 09:46:40 -0500

    Use custom build script to temporarily bump version and build

---
+++ scripts/versionAndBuild.mjs
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import { $ } from 'execa';
+
+async function main() {
+  const { stdout: status } = await $`git status --porcelain`;
+  if (status) {
+    console.error(`❗️ Working directory is not clean:\n${status}`);
+    return;
+  }
+
+  const { stdout: nextVersion } = await $`auto shipit --dry-run --quiet`;
+
+  console.info(`📌 Temporarily bumping version to '${nextVersion}' for build step`);
+  await $`npm --no-git-tag-version version ${nextVersion}`;
+
+  console.info('📦 Building with new version');
+  await $`yarn build`;
+
+  console.info('🧹 Resetting changes to let `auto` do its thing');
+  await $`git reset --hard`;
+
+  console.info('✅ Build with new version completed');
+}
+
+main();
