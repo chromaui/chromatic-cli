f4d4a945208fe311649fcc9895f8cb90fda70623
John Hobbs <john@velvetcache.org>
2024-10-17 10:57:58 -0500

    Style link in sentinel file error

--- node-src/ui/messages/errors/sentinelFileErrors.ts
+++ node-src/ui/messages/errors/sentinelFileErrors.ts
@@ -2,8 +2,9 @@ import chalk from 'chalk';
 import { dedent } from 'ts-dedent';
 
 import { error } from '../../components/icons';
+import link from '../../components/link';
 
 export default () =>
   dedent(chalk`
-    ${error} Failed to finalize upload. Please check https://status.chromatic.com/ or contact support.
+    ${error} Failed to finalize upload. Please check ${link('https://status.chromatic.com/')} or contact support.
   `);
