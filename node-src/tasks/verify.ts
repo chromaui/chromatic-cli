a51efd9c48410b38f6a98fe3007bb5c42e3bb417
Cody Kaup <dbkaup@gmail.com>
2024-12-05 10:17:20 -0600

    Account for flags when checking exitOnceUploaded

--- node-src/tasks/verify.ts
+++ node-src/tasks/verify.ts
@@ -270,7 +270,11 @@ export const verifyBuild = async (ctx: Context, task: Task) => {
 
   transitionTo(success, true)(ctx, task);
 
-  if (list || ctx.isPublishOnly || matchesBranch?.(ctx.options.exitOnceUploaded)) {
+  if (
+    list ||
+    ctx.isPublishOnly ||
+    matchesBranch?.(ctx.flags.exitOnceUploaded ?? ctx.options.exitOnceUploaded)
+  ) {
     setExitCode(ctx, exitCodes.OK);
     ctx.skipSnapshots = true;
   }
