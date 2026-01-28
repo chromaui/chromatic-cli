2baa50059018437f13daa7525dfba8eb3951bdab
Norbert de Langen <ndelangen@me.com>
2026-01-28 17:14:39 +0100

    simplify test-storybook config dir

--- .storybook-test/main.ts
+++ .storybook-test/main.ts
@@ -1,5 +1,4 @@
 import { StorybookConfig } from '@storybook/react-vite';
-import { fileURLToPath } from 'node:url';
 
 const config: StorybookConfig = {
   stories: ['../test-stories/*.stories.*'],
@@ -9,16 +8,6 @@ const config: StorybookConfig = {
   framework: { 
     name: '@storybook/react-vite', 
     options: {}
-  },
-  viteFinal: async (config) => {
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...config.resolve?.alias,
-        os: fileURLToPath(import.meta.resolve('os-browserify/browser')),
-      },
-    };
-    return config;
   },
   typescript: {
     reactDocgen: 'react-docgen-typescript',
