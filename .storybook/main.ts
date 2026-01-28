1e3ed9613dae8ec8568dcaf93888908e9731f88a
Norbert de Langen <ndelangen@me.com>
2026-01-28 13:44:54 +0100

    make the local storybook function again, switching to vite builder and html renderer

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,28 +1,71 @@
-import { StorybookConfig } from '@storybook/react-webpack5';
+import { StorybookConfig } from '@storybook/html-vite';
 import { fileURLToPath } from 'node:url';
 
 const config: StorybookConfig = {
   stories: process.env.SMOKE_TEST
     ? ['../test-stories/*.stories.*']
     : ['../node-src/**/*.@(mdx|stories.*)'],
-  addons: ['@storybook/addon-webpack5-compiler-swc', '@storybook/addon-docs'],
+  addons: [
+    '@storybook/addon-docs'
+  ],
   framework: { 
-    name: '@storybook/react-webpack5', 
+    name: '@storybook/html-vite', 
     options: {}
   },
-  webpackFinal: async (config) => {
+  viteFinal: async (config) => {
     config.resolve = {
       ...config.resolve,
-      fallback: {
-        ...config?.resolve?.fallback,
-        os:  fileURLToPath(import.meta.resolve('os-browserify/browser')),
-        path:  fileURLToPath(import.meta.resolve('path-browserify')),
+      alias: {
+        ...config.resolve?.alias,
+        os: fileURLToPath(import.meta.resolve('os-browserify/browser')),
       },
     };
     return config;
   },
+  previewHead: (head) => {
+    return head + `
+    <script>
+      window.process = {
+        env: {
+          CI: '1',
+          NODE_ENV: 'production',
+          STORYBOOK_INVOKED_BY: 'chromatic',
+          TERM: 'xterm-256color',
+        },
+        platform: '${process.platform}',
+      };
+    </script>
+
+    <link href="/css/global.css" rel="stylesheet" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
+
+    <style>
+      .code-style {
+        display: inline-block;
+        margin: 0;
+        padding: 1rem;
+        font-size: 12px;
+        font-family: "Source Code Pro", monospace;
+        white-space: pre-wrap;
+        line-height: 1rem;
+        color: #c0c4cd;
+      }
+
+      .html-style {
+        font-family: "Nunito Sans", sans-serif;
+        font-size: 14px;
+        line-height: 1;
+        color: #5C6870;
+        padding: 20px;
+      }
+    </style>
+    `;
+  },
   typescript: {
-    reactDocgen: 'react-docgen-typescript',
+    // reactDocgen: 'react-docgen-typescript',
   },
   staticDirs: ['../static'],
 };
