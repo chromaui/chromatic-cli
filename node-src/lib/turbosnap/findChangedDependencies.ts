37e6e20a884e06217799b42197bfc46ce4e97f5c
Cody Kaup <dbkaup@gmail.com>
2026-02-13 11:26:56 -0600

    TurboSnap: p-limit -> p-map

--- node-src/lib/turbosnap/findChangedDependencies.ts
+++ node-src/lib/turbosnap/findChangedDependencies.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import os from 'os';
-import pLimit from 'p-limit';
+import pMap from 'p-map';
 import path from 'path';
 
 import { checkoutFile, findFilesFromRepositoryRoot, getRepositoryRoot } from '../../git/git';
@@ -116,71 +116,62 @@ export const findChangedDependencies = async (ctx: Context) => {
   const changedDependencyNames = new Set<string>();
   const tmpdirsCreated = new Set<string>();
 
-  // Create limiter for package processing
-  const packageLimit = pLimit(packageConcurrency);
-
   try {
-    await Promise.all(
-      filteredPathPairs.map(([manifestPath, lockfilePath, commits]) =>
-        packageLimit(async () => {
-          // Create a temporary directory for the HEAD dependencies. We do this to isolate the
-          // package.json and lock files from the rest of the repository because the `inspect` function
-          // from `snyk-nodejs-plugin` used inside getDependencies.ts hardcodes the file paths based on
-          // the root path it receives (first argument).
-          const tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), 'chromatic'));
-          tmpdirsCreated.add(tmpdir);
+    await pMap(
+      filteredPathPairs,
+      async ([manifestPath, lockfilePath, commits]) => {
+        // Create a temporary directory for the HEAD dependencies. We do this to isolate the
+        // package.json and lock files from the rest of the repository because the `inspect` function
+        // from `snyk-nodejs-plugin` used inside getDependencies.ts hardcodes the file paths based on
+        // the root path it receives (first argument).
+        const tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), 'chromatic'));
+        tmpdirsCreated.add(tmpdir);
 
-          const absoluteManifestPath = path.join(rootPath, manifestPath);
-          const absoluteLockfilePath = path.join(rootPath, lockfilePath);
-          const temporaryManifestPath = path.join(tmpdir, path.basename(manifestPath));
-          const temporaryLockfilePath = path.join(tmpdir, path.basename(lockfilePath));
+        const absoluteManifestPath = path.join(rootPath, manifestPath);
+        const absoluteLockfilePath = path.join(rootPath, lockfilePath);
+        const temporaryManifestPath = path.join(tmpdir, path.basename(manifestPath));
+        const temporaryLockfilePath = path.join(tmpdir, path.basename(lockfilePath));
 
-          fs.copyFileSync(absoluteManifestPath, temporaryManifestPath);
-          fs.copyFileSync(absoluteLockfilePath, temporaryLockfilePath);
+        fs.copyFileSync(absoluteManifestPath, temporaryManifestPath);
+        fs.copyFileSync(absoluteLockfilePath, temporaryLockfilePath);
 
-          const headDependencies = await getDependencies(ctx, {
-            rootPath: tmpdir,
-            manifestPath: temporaryManifestPath,
-            lockfilePath: temporaryLockfilePath,
-          });
+        const headDependencies = await getDependencies(ctx, {
+          rootPath: tmpdir,
+          manifestPath: temporaryManifestPath,
+          lockfilePath: temporaryLockfilePath,
+        });
 
-          ctx.log.debug({ manifestPath, lockfilePath }, `Found HEAD dependencies`);
+        ctx.log.debug({ manifestPath, lockfilePath }, `Found HEAD dependencies`);
 
-          // Create limiter for baseline comparisons (per package)
-          const baselineLimit = pLimit(baselineConcurrency);
+        // Retrieve the union of dependencies which changed compared to each baseline.
+        // A change means either the version number is different or the dependency was added/removed.
+        // If a manifest or lockfile is missing on the baseline, this throws and we'll end up bailing.
+        await pMap(
+          commits,
+          async (reference) => {
+            // Create a temporary directory for the baseline dependencies to also isolate the
+            // package.json and lock files for the `inspect` function from `snyk-nodejs-plugin` in
+            // getDependencies.ts.
+            const tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), 'chromatic'));
+            tmpdirsCreated.add(tmpdir);
 
-          // Retrieve the union of dependencies which changed compared to each baseline.
-          // A change means either the version number is different or the dependency was added/removed.
-          // If a manifest or lockfile is missing on the baseline, this throws and we'll end up bailing.
-          await Promise.all(
-            commits.map((reference) =>
-              baselineLimit(async () => {
-                // Create a temporary directory for the baseline dependencies to also isolate the
-                // package.json and lock files for the `inspect` function from `snyk-nodejs-plugin` in
-                // getDependencies.ts.
-                const tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), 'chromatic'));
-                tmpdirsCreated.add(tmpdir);
+            const baselineDependencies = await getDependencies(ctx, {
+              rootPath: tmpdir,
+              manifestPath: await checkoutFile(ctx, reference, manifestPath, tmpdir),
+              lockfilePath: await checkoutFile(ctx, reference, lockfilePath, tmpdir),
+            });
 
-                const baselineDependencies = await getDependencies(ctx, {
-                  rootPath: tmpdir,
-                  manifestPath: await checkoutFile(ctx, reference, manifestPath, tmpdir),
-                  lockfilePath: await checkoutFile(ctx, reference, lockfilePath, tmpdir),
-                });
+            ctx.log.debug({ reference }, `Found baseline dependencies`);
 
-                ctx.log.debug({ reference }, `Found baseline dependencies`);
-
-                const baselineChanges = await compareBaseline(
-                  headDependencies,
-                  baselineDependencies
-                );
-                for (const change of baselineChanges) {
-                  changedDependencyNames.add(change);
-                }
-              })
-            )
-          );
-        })
-      )
+            const baselineChanges = await compareBaseline(headDependencies, baselineDependencies);
+            for (const change of baselineChanges) {
+              changedDependencyNames.add(change);
+            }
+          },
+          { concurrency: baselineConcurrency }
+        );
+      },
+      { concurrency: packageConcurrency }
     );
   } finally {
     for (const tmpdir of tmpdirsCreated) {
