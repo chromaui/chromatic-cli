639997915f2e1021a65bf81f394f351497f0fd53
Armando Vasquez <mando@mcnuggies.dev>
2026-04-17 14:42:44 -0500

    add example refs for testing

--- node-src/tasks/initialize.test.ts
+++ node-src/tasks/initialize.test.ts
@@ -105,7 +105,14 @@ describe('announceBuild', () => {
     environment: ':environment',
     git: { version: 'whatever', matchesBranch: () => false, committedAt: 0 },
     pkg: { version: '1.0.0' },
-    storybook: { baseDir: '', version: '2.0.0', addons: [] },
+    storybook: {
+      baseDir: '',
+      version: '2.0.0',
+      addons: [],
+      refs: {
+        design: { title: 'Design System', url: 'https://design.example.com' },
+      },
+    },
     runtimeMetadata: {
       nodePlatform: 'darwin',
       nodeVersion: '18.12.1',
