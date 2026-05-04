66e5706b9903e11b7d4e42d62398d5f7b794b141
Tom Coleman <tom@chromatic.com>
2026-05-04 17:22:01 +1000

    Add test for external storybook configs

--- node-src/lib/turbosnap/getDependentStoryFiles.test.ts
+++ node-src/lib/turbosnap/getDependentStoryFiles.test.ts
@@ -528,6 +528,28 @@ describe('getDependentStoryFiles', () => {
     );
   });
 
+    it('does not bail on changed external Storybook config file', async () => {
+    const changedFiles = ['src/foo.stories.js', 'path/to/other-storybook-config/file.js'];
+    const modules = [
+      {
+        id: './src/foo.stories.js',
+        name: './src/foo.stories.js',
+        reasons: [{ moduleName: CSF_GLOB }],
+      },
+      {
+        id: CSF_GLOB,
+        name: CSF_GLOB,
+        reasons: [{ moduleName: './path/to/storybook-config/generated-stories-entry.js' }],
+      },
+    ];
+    const ctx = getContext({ configDir: 'path/to/storybook-config' });
+    const result = await getDependentStoryFiles(ctx, { modules }, statsPath, changedFiles);
+    expect(ctx.turboSnap.bailReason).toBeUndefined();
+    expect(result).toEqual({
+      './src/foo.stories.js': ['src/foo.stories.js'],
+    });
+  });
+
   it('bails on changed preview.js file', async () => {
     const changedFiles = ['src/foo.stories.js', 'path/to/storybook-config/preview.js'];
     const modules = [
