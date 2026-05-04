4fc537d00c5c7f6a6d0ea1036b5608c7262611dc
John Hobbs <john@velvetcache.org>
2026-05-04 10:41:39 -0500

    Remove duplicate RN build stories

--- node-src/ui/tasks/build.stories.ts
+++ node-src/ui/tasks/build.stories.ts
@@ -1,13 +1,5 @@
 import task from '../components/task';
 import { failed, initial, pending, skipped, success } from './build';
-import {
-  failed as failedReactNative,
-  pendingAndroid,
-  pendingIOS,
-  pendingManifest,
-  skipped as skippedReactNative,
-  success as successReactNative,
-} from './buildReactNative';
 
 export default {
   title: 'CLI/Tasks/Build',
@@ -37,37 +29,3 @@ export const Skipped = () =>
   } as any);
 
 export const Failed = () => failed({ ...ctx, buildCommand } as any);
-
-export const BuiltReactNative = () =>
-  successReactNative({
-    ...ctx,
-    now: 0,
-    startedAt: -32_100,
-    reactNativeBuildLogFile: '/users/me/project/storybook-static/.chromatic/react-native-build.log',
-  } as any);
-
-export const SkippedForReactNative = () => skippedReactNative();
-
-export const BuildingAndroid = () => pendingAndroid({ ...ctx } as any);
-
-export const BuildingAndroidWithCommand = () =>
-  pendingAndroid({
-    ...ctx,
-    options: { reactNative: { androidBuildCommand: 'my-android-build' } },
-  } as any);
-
-export const BuildingIOS = () => pendingIOS({ ...ctx } as any);
-
-export const BuildingIOSWithCommand = () =>
-  pendingIOS({
-    ...ctx,
-    options: { reactNative: { iosBuildCommand: 'my-ios-build' } },
-  } as any);
-
-export const GeneratingManifest = () => pendingManifest();
-
-export const FailedReactNative = () =>
-  failedReactNative({
-    ...ctx,
-    reactNativeBuildLogFile: '/path/to/project/storybook-static/.chromatic/react-native-build.log',
-  } as any);
