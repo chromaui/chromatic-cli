661dea23c38c73100a5e404503dc2405e38cca35
Justin Thurman <thurman.justin@gmail.com>
2026-05-11 09:20:08 -0400

    Refactor validateFiles to return file info

--- node-src/tasks/prepare/index.ts
+++ node-src/tasks/prepare/index.ts
@@ -1,18 +1,31 @@
+import type Listr from 'listr';
+
 import { createTask, transitionTo } from '../../lib/tasks';
-import { Context, FileInfo, TaskResult } from '../../types';
-import { initial, success, validating } from '../../ui/tasks/prepare';
+import { AnnouncedBuild, Context, FileInfo, TaskResult } from '../../types';
+import {
+  hashing,
+  initial,
+  invalid,
+  invalidReactNative,
+  success,
+  validating,
+} from '../../ui/tasks/prepare';
 import { calculateFileHashes, CalculateFileHashesInput } from './calculateFileHashes';
 import { traceChangedFiles } from './traceChangedFiles';
 import { validateAndroidArtifact } from './validateAndroidArtifact';
-import { validateFiles } from './validateFiles';
+import {
+  isValidReactNativeStorybook,
+  isValidStorybook,
+  validateFiles,
+  ValidateFilesInput,
+} from './validateFiles';
 
-type PrepareDeps = Pick<Context, 'log' | 'env' | 'options'>;
+type PrepareDeps = Pick<Context, 'log' | 'env' | 'options' | 'packageJson'>;
 
 interface PrepareInput {
-  transitionToHashing: () => void;
-  invalidFileInfoError: Error;
-  fileInfo?: FileInfo;
   sourceDir: string;
+  validateFilesInput: ValidateFilesInput;
+  transitionToHashing: () => void;
 }
 
 interface PrepareOutput {
@@ -24,27 +37,63 @@ export async function runPrepare(
   deps: PrepareDeps,
   input: PrepareInput
 ): Promise<TaskResult<PrepareOutput>> {
+  const fileInfo = await validateFiles(deps, input.validateFilesInput);
+
   let hashes: Record<string, string> | undefined;
   if (deps.options.fileHashing) {
     input.transitionToHashing();
-    if (input.fileInfo) {
-      const calculateFileHashesInput: CalculateFileHashesInput = {
-        fileInfo: input.fileInfo,
-        sourceDir: input.sourceDir,
-      };
-      try {
-        hashes = await calculateFileHashes(deps, calculateFileHashesInput);
-      } catch (err) {
-        deps.log.warn('Failed to calculate file hashes');
-        deps.log.debug(err);
-      }
-    } else {
-      deps.log.warn('Failed to calculate file hashes: no file info available.');
-      deps.log.debug(input.invalidFileInfoError);
+    const calculateFileHashesInput: CalculateFileHashesInput = {
+      fileInfo,
+      sourceDir: input.sourceDir,
+    };
+    try {
+      hashes = await calculateFileHashes(deps, calculateFileHashesInput);
+    } catch (err) {
+      deps.log.warn('Failed to calculate file hashes');
+      deps.log.debug(err);
     }
   }
   return { kind: 'continue', output: { hashes } };
 }
+
+export const extractPrepareInput = (
+  ctx: Context,
+  listrTask: Listr.ListrTaskWrapper<Context>
+): PrepareInput => {
+  if (!ctx.announcedBuild) {
+    throw new Error('Announced build required for prepare task.');
+  }
+  // eslint-disable-next-line unicorn/prevent-abbreviations
+  const sourceDir = ctx.sourceDir;
+  const validateFilesInput: ValidateFilesInput = {
+    browsers: ctx.announcedBuild.browsers,
+    buildLogFile: ctx.buildLogFile,
+    getFileInfoErrorBuilder(err: Error): Error {
+      return new Error(invalid(ctx, err).output);
+    },
+    isReactNativeApp: ctx.isReactNativeApp || false,
+    sourceDir,
+    validationErrorBuilder(missingFiles: string[] | undefined): Error {
+      return ctx.isReactNativeApp
+        ? new Error(invalidReactNative(ctx, missingFiles).output)
+        : new Error(invalid(ctx).output);
+    },
+    validator(
+      fileInfo: FileInfo,
+      browsers: AnnouncedBuild['browsers']
+    ): { valid: boolean; missingFiles: string[] } {
+      return ctx.isReactNativeApp
+        ? isValidReactNativeStorybook(fileInfo, browsers)
+        : isValidStorybook(fileInfo);
+    },
+  };
+
+  return {
+    sourceDir,
+    validateFilesInput,
+    transitionToHashing: () => transitionTo(hashing)(ctx, listrTask),
+  };
+};
 
 /**
  * Sets up the Listr task for preparing the built storybook for upload to Chromatic.
