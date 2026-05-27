e28ddd2dc3d5a9d12dd7cddd6f3fe67146abb5e5
Cody Kaup <dbkaup@gmail.com>
2026-05-27 11:34:26 -0500

    Add error type to getChangedFiles

--- node-src/git/git.ts
+++ node-src/git/git.ts
@@ -4,7 +4,11 @@ import pLimit from 'p-limit';
 import path from 'path';
 import { file as temporaryFile } from 'tmp-promise';
 
-import { BaselineCheckoutFailedError } from '../lib/turbosnap/errors';
+import {
+  AncestorMissingError,
+  BaselineCheckoutFailedError,
+  GitCommandError,
+} from '../lib/turbosnap/errors';
 import { Deps } from '../types';
 import { execGitCommand, execGitCommandCountLines, execGitCommandOneLine } from './execGit';
 
@@ -204,11 +208,16 @@ export async function getChangedFiles(
   headCommit = ''
 ) {
   // Note that an empty headCommit will include uncommitted (staged or unstaged) changes.
-  const files = await execGitCommand(
-    deps,
-    `git --no-pager diff --name-only --no-relative ${baseCommit} ${headCommit}`
-  );
-  return files?.split(newline).filter(Boolean);
+  const command = `git --no-pager diff --name-only --no-relative ${baseCommit} ${headCommit}`;
+  try {
+    const files = await execGitCommand(deps, command);
+    return files?.split(newline).filter(Boolean);
+  } catch (error) {
+    if (/bad object/.test(error.message)) {
+      throw new AncestorMissingError(baseCommit, { cause: error });
+    }
+    throw new GitCommandError(command, { cause: error });
+  }
 }
 
 /**
