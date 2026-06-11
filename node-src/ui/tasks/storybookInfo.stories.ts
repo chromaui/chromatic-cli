711f87a028d12cc824e42884b639e7f170ef3a0b
Justin Thurman <thurman.justin@gmail.com>
2026-06-11 09:09:25 -0400

    Render storybookInfo stories with Clack renderer

--- node-src/ui/tasks/storybookInfo.stories.ts
+++ node-src/ui/tasks/storybookInfo.stories.ts
@@ -1,21 +1,8 @@
-import task from '../components/task';
-import { initial, pending, success } from './storybookInfo';
+import frames from './storybookInfo.frames?clack';
 
 export default {
   title: 'CLI/Tasks/StorybookInfo',
-  decorators: [(storyFunction: any) => task(storyFunction())],
 };
 
-const storybook = {
-  version: '5.3.0',
-  builder: { name: 'webpack4', packageVersion: '5.3.0' },
-  addons: [],
-};
-
-const ctx = { options: {} } as any;
-
-export const Initial = () => initial(ctx);
-
-export const Pending = () => pending(ctx);
-
-export const Success = () => success({ ...ctx, storybook } as any);
+export const Pending = () => frames.Pending;
+export const Success = () => frames.Success;
