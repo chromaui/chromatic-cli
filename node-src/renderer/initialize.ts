a82f2ee8dc2f66e1284666083e890942973bad92
Justin Thurman <thurman.justin@gmail.com>
2026-06-12 10:41:43 -0400

    Migrate initialize task to Clack renderer

---
+++ node-src/renderer/initialize.ts
@@ -0,0 +1,26 @@
+import { applyInitializeOutput, extractInitializeInput, initialize } from '../tasks/initialize';
+import { Context } from '../types';
+import { initial, pending, success } from '../ui/tasks/initialize';
+import { runTask } from './engine';
+import { clackTaskLogRenderer } from './engine/clack/renderer';
+import { getRenderer } from './engine/getRenderer';
+
+/**
+ * Render the initialize task.
+ *
+ * @param ctx The CLI context.
+ */
+export async function renderInitialize(ctx: Context): Promise<void> {
+  await runTask(
+    ctx,
+    {
+      name: 'initialize',
+      title: initial.title,
+      transitions: { pending, success },
+      extractInput: extractInitializeInput,
+      run: initialize,
+      applyOutput: applyInitializeOutput,
+    },
+    getRenderer(ctx, clackTaskLogRenderer)
+  );
+}
