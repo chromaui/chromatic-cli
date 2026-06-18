01cad42f1048547efddb3392f0f8d08108c92156
Justin Thurman <thurman.justin@gmail.com>
2026-06-18 10:04:48 -0400

    Refactor setSourceDirectory to (deps, input) -> sourceDir

--- node-src/tasks/build/setSourceDirectory.ts
+++ node-src/tasks/build/setSourceDirectory.ts
@@ -1,20 +1,29 @@
 import semver from 'semver';
 import tmp from 'tmp-promise';
 
-import { Context } from '../../types';
+import { Context, Deps } from '../../types';
 
-export const setSourceDirectory = async (ctx: Context) => {
-  // do not overwrite if it is already set, for instance in
-  // the skip condition of the build task on React Native
-  if (ctx.sourceDir) return;
+type SetSourceDirectoryDeps = Pick<Deps, 'options'>;
 
-  if (ctx.options.outputDir) {
-    ctx.sourceDir = ctx.options.outputDir;
-  } else if (ctx.storybook && ctx.storybook.version && semver.lt(ctx.storybook.version, '5.0.0')) {
-    // Storybook v4 doesn't support absolute paths like tmp.dir would yield
-    ctx.sourceDir = 'storybook-static';
-  } else {
-    const temporaryDirectory = await tmp.dir({ unsafeCleanup: true, prefix: `chromatic-` });
-    ctx.sourceDir = temporaryDirectory.path;
+interface SetSourceDirectoryInput {
+  // Preset by the build task's React Native skip path; when present we keep it as-is.
+  sourceDir?: string;
+  storybook?: Context['storybook'];
+}
+
+export const setSourceDirectory = async (
+  deps: SetSourceDirectoryDeps,
+  input: SetSourceDirectoryInput
+): Promise<string> => {
+  if (input.sourceDir) return input.sourceDir;
+
+  if (deps.options.outputDir) return deps.options.outputDir;
+
+  // Storybook v4 doesn't support absolute paths like tmp.dir would yield
+  if (input.storybook?.version && semver.lt(input.storybook.version, '5.0.0')) {
+    return 'storybook-static';
   }
+
+  const temporaryDirectory = await tmp.dir({ unsafeCleanup: true, prefix: `chromatic-` });
+  return temporaryDirectory.path;
 };
