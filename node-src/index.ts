f2272986c8618ffe70648b9d7dfe0920dabc33c7
Justin Thurman <thurman.justin@gmail.com>
2026-06-18 11:10:18 -0400

    Swap build task onto runTask + spinner renderer

--- node-src/index.ts
+++ node-src/index.ts
@@ -33,6 +33,7 @@ import { rewriteErrorMessage } from './lib/utilities';
 import { writeChromaticDiagnostics } from './lib/writeChromaticDiagnostics';
 import { intro as clackIntro } from './renderer';
 import { renderAuth } from './renderer/auth';
+import { renderBuild } from './renderer/build';
 import { renderGitInfo } from './renderer/gitInfo';
 import { renderInitialize } from './renderer/initialize';
 import { renderStorybookInfo } from './renderer/storybookInfo';
@@ -294,6 +295,7 @@ async function runBuild(ctx: Context) {
       await renderGitInfo(ctx);
       await renderStorybookInfo(ctx);
       await renderInitialize(ctx);
+      await renderBuild(ctx);
       await new Listr(getTasks(ctx), options).run(ctx);
       ctx.log.debug('Tasks completed');
     } catch (err) {
